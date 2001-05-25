2cdf6f7ae67e008455968348ea6b3d9d9ab3c320
joepeer <>
2001-05-25 07:19:23 +0000

    adding enhancements by Philip Grunikiewicz: a Textfield can have a value independent from the database and it can be set to  be hidden (new attributes in taglib-descriptor: "overrideValue", "hidden")

--- dbforms/src/com/itp/dbforms/taglib/DbTextFieldTag.java
+++ dbforms/src/com/itp/dbforms/taglib/DbTextFieldTag.java
@@ -50,67 +50,154 @@ public class DbTextFieldTag extends DbBaseInputTag  {
   static Category logCat = Category.getInstance(DbTextFieldTag.class.getName()); // logging category for this class
 
   public int doStartTag() throws javax.servlet.jsp.JspException {
-    return SKIP_BODY;
-  }
+	return SKIP_BODY;
+  }  
 
-  public int doEndTag() throws javax.servlet.jsp.JspException {
+/* ===========================================================================
+ * grunikiewicz.philip@hydro.qc.ca
+ * 2001-05-23
+ *
+ * Added two new attributes: hidden and overrideValue
+ *
+ *	hidden : 		Determines if the text field should be hidden or displayed.
+ *	overrideValue : Defines the text field's default value. When this attribute is set, the value retrieved
+ * 					from the database is ignored.
+ *
+ * These attributes where added to allow a developer to:
+ *		- set a field's default value (Insert mode)
+ *		- override a field's value with a developer defined value (Update mode)
+ *		- hide a field, set its value (Insert and Update mode)
+ *
+ *	Example:
+ *
+ *	From within a given JSP page, a developer is required to set the SOLD field to 'true' when the user clicks
+ *	the updateButton.  The SOLD field should not to be displayed on screen.
+ *
+ *		<db:textField fieldName="SOLD" hidden="true" overrideValue="true"/>
+ *		<db:updateButton caption="OK"/>
+ *
+*/
 
+public int doEndTag() throws javax.servlet.jsp.JspException {
 
-		try {
+	try {
 
-      StringBuffer tagBuf = new StringBuffer("<input type=\"text\" name=\"");
-      //if(parsedSearchMode == DbBaseHandlerTag.SEARCHMODE_NONE) {
-      	tagBuf.append(getFormFieldName());
-  	  //} else {
+		/* Does the developer require the field to be hidden or displayed? */
+		String value =
+			("true".equals(this.getHidden()))
+				? "<input type=\"hidden\" name=\""
+				: "<input type=\"text\" name=\"";
+
+		StringBuffer tagBuf = new StringBuffer(value);
+		//if(parsedSearchMode == DbBaseHandlerTag.SEARCHMODE_NONE) {
+		tagBuf.append(getFormFieldName());
+		//} else {
 		//tagBuf.append(getSearchFieldName());
-	  //}
-      tagBuf.append("\" value=\"");
-      //if(parsedSearchMode == DbBaseHandlerTag.SEARCHMODE_NONE) {
-	  	tagBuf.append(getFormFieldValue());
-  	  //}
-      tagBuf.append("\" ");
+		//}
+		tagBuf.append("\" value=\"");
+		//if(parsedSearchMode == DbBaseHandlerTag.SEARCHMODE_NONE) {
 
-			if (accessKey != null) {
-			    tagBuf.append(" accesskey=\"");
-			    tagBuf.append(accessKey);
-			    tagBuf.append("\"");
-			}
+		/* If the overrideValue attribute has been set, use its value instead of the one
+			retrieved from the database.  This mechanism can be used to set an initial default
+			value for a given field. */
 
-			if (maxlength != null) {
-		    tagBuf.append(" maxlength=\"");
-		    tagBuf.append(maxlength);
-		    tagBuf.append("\"");
-			}
+		if (this.getOverrideValue() != null) {
+			tagBuf.append(this.getOverrideValue());
+		} else {
+			tagBuf.append(getFormFieldValue());
+		}
+		//}
+		tagBuf.append("\" ");
 
-			if (cols != null) {
-		    tagBuf.append(" size=\"");
-		    tagBuf.append(cols);
-		    tagBuf.append("\"");
-			}
-
-			if (tabIndex != null) {
-		    tagBuf.append(" tabindex=\"");
-		    tagBuf.append(tabIndex);
-		    tagBuf.append("\"");
-			}
-
-      tagBuf.append(prepareStyles());
-      tagBuf.append(prepareEventHandlers());
-      tagBuf.append(">");
-
-      //if(parsedSearchMode != DbBaseHandlerTag.SEARCHMODE_NONE) {
-		//tagBuf.append(getSearchModeTag());
-	  //}
-
-
-		  pageContext.getOut().write(tagBuf.toString());
-		} catch(java.io.IOException ioe) {
-			throw new JspException("IO Error: "+ioe.getMessage());
+		if (accessKey != null) {
+			tagBuf.append(" accesskey=\"");
+			tagBuf.append(accessKey);
+			tagBuf.append("\"");
 		}
 
-		return EVAL_PAGE;
-  }
+		if (maxlength != null) {
+			tagBuf.append(" maxlength=\"");
+			tagBuf.append(maxlength);
+			tagBuf.append("\"");
+		}
 
+		if (cols != null) {
+			tagBuf.append(" size=\"");
+			tagBuf.append(cols);
+			tagBuf.append("\"");
+		}
+
+		if (tabIndex != null) {
+			tagBuf.append(" tabindex=\"");
+			tagBuf.append(tabIndex);
+			tagBuf.append("\"");
+		}
+
+		tagBuf.append(prepareStyles());
+		tagBuf.append(prepareEventHandlers());
+		tagBuf.append(">");
+
+		//if(parsedSearchMode != DbBaseHandlerTag.SEARCHMODE_NONE) {
+		//tagBuf.append(getSearchModeTag());
+		//}
+
+		pageContext.getOut().write(tagBuf.toString());
+	} catch (java.io.IOException ioe) {
+		throw new JspException("IO Error: " + ioe.getMessage());
+	}
+
+	return EVAL_PAGE;
 }
 
+	private java.lang.String hidden = "false";
+	private java.lang.String overrideValue = null;
 
+/**
+ * grunikiewicz.philip@hydro.qc.ca
+ * 2001-05-23
+ * @param newHidden java.lang.String
+ *
+ * Determines if the text field should be hidden or displayed
+ * @return java.lang.String
+ */
+public java.lang.String getHidden() {
+	return hidden;
+}
+
+/**
+ * grunikiewicz.philip@hydro.qc.ca
+ * 2001-05-23
+ * 
+ * Defines the text field's default value. When this attribute is set, the value retrieved
+ * from the database is ignored.
+ *
+ * @return java.lang.String
+ */
+public java.lang.String getOverrideValue() {
+	return overrideValue;
+}
+
+/**
+ * grunikiewicz.philip@hydro.qc.ca
+ * 2001-05-23
+ * @param newHidden java.lang.String
+ *
+ * Determines if the text field should be hidden or displayed
+ */
+public void setHidden(java.lang.String newHidden) {
+	hidden = newHidden;
+}
+
+/**
+ * grunikiewicz.philip@hydro.qc.ca
+ * 2001-05-23
+ * 
+ * Defines the text field's default value. When this attribute is set, the value retrieved
+ * from the database is ignored.
+ *
+ * @param newOverrideValue java.lang.String
+ */
+public void setOverrideValue(java.lang.String newOverrideValue) {
+	overrideValue = newOverrideValue;
+}
+}
