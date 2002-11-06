e9c2ae8802c47e6f6fb4a851867c3141cb02bb7c
foxat <>
2002-11-06 10:20:59 +0000

    added Bradley's multiple db connection support added Pow2 connectionPool factory support (disabled)

--- dbforms/src/org/dbforms/util/DbConnection.java
+++ dbforms/src/org/dbforms/util/DbConnection.java
@@ -30,6 +30,11 @@ import javax.naming.*;
 
 import org.apache.log4j.Category;
 
+/* POW2 FUNCTIONALITY
+import com.pow2.dao.ConnectionFactory;
+import com.pow2.dao.ConnectionProviderPrefs;
+POW2 FUNCTIONALITY */
+
 /****
  * <p>
  * this class represents datastructures like to following examples:
@@ -72,12 +77,70 @@ public class DbConnection {
 
    static Category logCat = Category.getInstance(DbConnection.class.getName()); // logging category for this class
 
+	private String id;
 	private String name;
-	private String isJndi;
-	private boolean jndi;
+	private String isJndi = "false";
+	private boolean jndi = false;
+	private boolean defaultConnection = false;
 	private String conClass;
 	private String username;
 	private String password;
+
+
+	private static final String CONNECTION_FACTORY_CLASS 
+		= "com.pow2.dao.ConnectionFactory";
+/* POW2 FUNCTIONALITY
+        private ConnectionFactory connectionFactory 
+		= ConnectionFactory.instance();
+POW2 FUNCTIONALITY */
+	private String connectionProviderClass;
+	private String connectionPoolURL;
+	private String isPow2 = "false";
+	private boolean pow2 = false;
+	private boolean isFactorySetup = false;
+
+	public void setConnectionProviderClass(String cpc) {
+		connectionProviderClass = cpc;
+	}
+
+	public String getConnectionProviderClass() {
+		return connectionProviderClass;
+	}
+
+	public void setConnectionPoolURL(String url) {
+		connectionPoolURL = url;
+	}
+
+	public String getConnectionPoolURL() {
+		return connectionPoolURL;
+	}
+
+	public void setIsPow2(String isPow2) {
+		this.isPow2 = isPow2;
+
+		pow2 = new Boolean(isPow2).booleanValue();
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setDefaultConnection(String defaultConnection) {
+		this.defaultConnection = new Boolean(defaultConnection).booleanValue();
+	}
+
+	public boolean isDefaultConnection() {
+		return defaultConnection;
+	}
+    
+    public String getDefaultConnection() {
+        return new Boolean(defaultConnection).toString();
+    }
 
 	public void setName(String name) {
 		this.name = name;
@@ -121,49 +184,96 @@ public class DbConnection {
 	}
 
 	public Connection getConnection() {
+		Connection con = null;
 
 		logCat.debug("returning a connection:"+this.toString());
 
 
-	  // access Connection via Application Server's JNDI table
-	  if(jndi) {
+		// access Connection via Application Server's JNDI table
+		if(jndi) {
 
-	  try {
+			try {
 				Context ctx = new InitialContext();
 				DataSource ds = (DataSource) ctx.lookup(name);
-				Connection con = ds.getConnection();
-				return con;
+				con = ds.getConnection();
 			} catch(NamingException ne) {
 				ne.printStackTrace();
-   	    return null;
-	  	} catch(Exception e) {
+				return null;
+	  		} catch(Exception e) {
 				e.printStackTrace();
-   	    return null;
-	  	}
+   	    			return null;
+	  		}
+
+
+		// access the connection using the pow2 library by Luca Fossato.
+/* POW2 FUNCTIONALITY
+                } else if (pow2) {
+			try {
+				if (!isFactorySetup) {
+					setupConnectionFactory();
+				}
+				con = connectionFactory.getConnection();
+			} catch (Exception se) {
+				logCat.error("::getConnection - cannot retrieve "
+					     + "a connection from the "
+					     + "connectionFactory", se);
+				return null;
+			}
+POW2 FUNCTIONALITY */
 
 		// access connection directly from db or from a connectionpool-manager like "Poolman"
 		} else {
 
-	    try {
- 	  		Class.forName(conClass).newInstance();
-			if (username != null) {
-				return DriverManager.getConnection(name, username, password);
-			}
-	  		return DriverManager.getConnection(name);
-	  	} catch(Exception e) {
+			try {
+ 	  			Class.forName(conClass).newInstance();
+				if (username != null) {
+					return DriverManager
+						 .getConnection(name, 
+								username, 
+								password);
+				}
+		  		con = DriverManager.getConnection(name);
+	  		} catch(Exception e) {
 				e.printStackTrace();
-   	    return null;
-	  	}
+   	    			return null;
+	  		}
 
 		}
+
+		return con;
 	}
+
+/* POW2 FUNCTIONALITY
+  	public void setupConnectionFactory() throws Exception {
+		ConnectionProviderPrefs prefs = new ConnectionProviderPrefs();
+
+		prefs.setConnectionProviderClass(connectionProviderClass);
+		prefs.setConnectionPoolURL(connectionPoolURL);
+		prefs.setJdbcDriver(conClass);
+		prefs.setJdbcURL(name);
+		prefs.setUser(username);
+		prefs.setPassword(password);
+
+		connectionFactory.setProvider(prefs);
+
+		isFactorySetup = true;
+  	}
+POW2 FUNCTIONALITY */
 
   public String toString() {
 	 StringBuffer buf = new StringBuffer("DbConnection = ");
-	 buf.append("name="+name);
+	 buf.append("id="+id);
+	 buf.append(",name="+name);
 	 buf.append(",jndi="+isJndi);
 	 buf.append(",conClass="+conClass);
 	 buf.append(",username="+username);
+	 buf.append(",default="+defaultConnection);
+
+	 if (pow2) {
+		buf.append(",connectionProviderClass"+connectionProviderClass);
+		buf.append(",connectionPoolURL"+connectionPoolURL);
+	 }
+
 	 //buf.append(",password="+password);  Not such a good idea!
 	 return buf.toString();
 
