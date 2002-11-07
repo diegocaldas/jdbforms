bf1e19adefbde74ad46f24b5e29af89d6db06860
foxat <>
2002-11-07 12:02:29 +0000

    Added the "defaultValue" attribute and the related accessor methods.
    Now it's possible to specify a  "default value" for an image name.
    For example, the name of your own "no-thumbnail-avaiable" icon ...
    
    Example:
    
    <db:blobURL fieldName="THUMBNAIL" defaultValue="myImage.jpg"/>
    
    can be used with ->
    
    <img src="<db:blobURL fieldName="THUMBNAIL" defaultValue="myImage.jpg"/>" border="0" width="xx"
    height="yy">
    
    In that manner, when the thumbnail image name is not
    avaiable (the related db field is blank), the FileServlet
    uses the specified "defaultValue attribute" value as the
    image name.
    
    See also the related changes into Fileservlet class

--- dbforms/src/org/dbforms/taglib/DbBlobURLTag.java
+++ dbforms/src/org/dbforms/taglib/DbBlobURLTag.java
@@ -20,7 +20,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
-
 package org.dbforms.taglib;
 
 import javax.servlet.http.*;
@@ -32,105 +31,171 @@ import org.apache.log4j.Category;
 /**
  * #fixme docu to come
  *
- * @author Joe Peer
+ * @author  Joe Peer
+ * @created  06 August 2002
  */
+public class DbBlobURLTag extends BodyTagSupport
+{
+  // logging category for this class
+  static Category logCat = Category.getInstance(DbBlobURLTag.class.getName());
 
-public class DbBlobURLTag extends BodyTagSupport {
+  // ----------------------------------------------------- Instance Variables
+  // DbForms specific
 
-	static Category logCat = Category.getInstance(DbBlobURLTag.class.getName()); // logging category for this class
-
-	// ----------------------------------------------------- Instance Variables
-
-
-// DbForms specific
-
-	protected DbFormsConfig config;
-  	protected String fieldName;
-  	protected Field field;
-
-		protected DbFormTag parentForm;
-
-	  public void setFieldName(String fieldName) {
-	    this.fieldName=fieldName;
-			this.field = parentForm.getTable().getFieldByName(fieldName);
-
-			//if(parentForm.isSubForm()) {
-				// tell parent that _this_ class will generate the html tag, not DbBodyTag!
-			//	parentForm.strikeOut(this.field);
-			//}
-	  }
-
-	  public String getFieldName() {
-	    return fieldName;
-	  }
-
-
-	// --------------------------------------------------------- Public Methods
-
-
-	// DbForms specific
-
-	  public void setPageContext(final javax.servlet.jsp.PageContext pageContext)  {
-	    super.setPageContext(pageContext);
-	    config = (DbFormsConfig) pageContext.getServletContext().getAttribute(DbFormsConfig.CONFIG);
-	  }
-
-	  public void setParent(final javax.servlet.jsp.tagext.Tag parent) {
-	    super.setParent(parent);
-	    //parentForm = (DbFormTag) getParent().getParent(); // between this form and its parent lies a DbHeader/Body/Footer-Tag!
-	    parentForm = (DbFormTag) findAncestorWithClass(this, DbFormTag.class);
-	  }
-
-	/**
-	 * Release any acquired resources.
-	 */
-	public void release() {
-			super.release();
-	}
-
-  public int doEndTag() throws javax.servlet.jsp.JspException {
-
-		try {
-
-
-
-  	  StringBuffer tagBuf = new StringBuffer(((HttpServletRequest) pageContext.getRequest()).getContextPath());
-  	  tagBuf.append("/servlet/file?tf=");
-	  tagBuf.append(getTableFieldCode());
-			tagBuf.append("&keyval=");
-			tagBuf.append(getKeyVal());
-		  pageContext.getOut().write(tagBuf.toString());
-		} catch(java.io.IOException ioe) {
-			throw new JspException("IO Error: "+ioe.getMessage());
-		}
-
-		return EVAL_PAGE;
-  }  
-
-
-	// ------------------------------------------------------ Protected Methods
-
-
-
-// DbForms specific
-
-
+  protected DbFormsConfig config;
+  protected String fieldName;
+  protected String defaultValue;
+  protected Field field;
+  protected DbFormTag parentForm;
 
 
   /**
-  generates the decoded name .
-  */
-	protected String getTableFieldCode() {
-		StringBuffer buf = new StringBuffer();
-	buf.append(parentForm.getTable().getId());
-	buf.append("_");
-	buf.append(field.getId());
-		return buf.toString();
-	}
-
-	protected String getKeyVal() {
-		return parentForm.getTable().getKeyPositionString(parentForm.getResultSetVector());
-	}
+   *  Gets the defaultValue attribute of the DbBlobURLTag object
+   *
+   * @return  The defaultValue value
+   */
+  public String getDefaultValue()
+  {
+    return defaultValue;
+  }
 
 
+  /**
+   *  Sets the defaultValue attribute of the DbBlobURLTag object
+   *
+   * @param  defaultValue The new defaultValue value
+   */
+  public void setDefaultValue(String defaultValue)
+  {
+    this.defaultValue = defaultValue;
+  }
+
+
+  /**
+   *  Sets the fieldName attribute of the DbBlobURLTag object
+   *
+   * @param  fieldName The new fieldName value
+   */
+  public void setFieldName(String fieldName)
+  {
+    this.fieldName = fieldName;
+    this.field = parentForm.getTable().getFieldByName(fieldName);
+
+    //if(parentForm.isSubForm()) {
+    // tell parent that _this_ class will generate the html tag, not DbBodyTag!
+    //  parentForm.strikeOut(this.field);
+    //}
+  }
+
+
+  /**
+   *  Gets the fieldName attribute of the DbBlobURLTag object
+   *
+   * @return  The fieldName value
+   */
+  public String getFieldName()
+  {
+    return fieldName;
+  }
+
+
+  // --------------------------------------------------------- Public Methods
+  // DbForms specific
+
+  /**
+   *  Sets the pageContext attribute of the DbBlobURLTag object
+   *
+   * @param  pageContext The new pageContext value
+   */
+  public void setPageContext(final javax.servlet.jsp.PageContext pageContext)
+  {
+    super.setPageContext(pageContext);
+    config = (DbFormsConfig) pageContext.getServletContext().getAttribute(DbFormsConfig.CONFIG);
+  }
+
+
+  /**
+   *  Sets the parent attribute of the DbBlobURLTag object
+   *
+   * @param  parent The new parent value
+   */
+  public void setParent(final javax.servlet.jsp.tagext.Tag parent)
+  {
+    super.setParent(parent);
+    //parentForm = (DbFormTag) getParent().getParent(); // between this form and its parent lies a DbHeader/Body/Footer-Tag!
+    parentForm = (DbFormTag) findAncestorWithClass(this, DbFormTag.class);
+  }
+
+
+  /**  Release any acquired resources. */
+  public void release()
+  {
+    super.release();
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   * @return  Description of the Return Value
+   * @exception  javax.servlet.jsp.JspException Description of the Exception
+   */
+  public int doEndTag() throws javax.servlet.jsp.JspException
+  {
+    try
+    {
+      StringBuffer tagBuf  = new StringBuffer(((HttpServletRequest) pageContext.getRequest()).getContextPath());
+
+      tagBuf.append("/servlet/file?tf=")
+            .append(getTableFieldCode())
+            .append("&keyval=")
+            .append(getKeyVal());
+
+      // append the defaultValue parameter;
+      if (defaultValue != null)
+      {
+        tagBuf.append("&defaultValue=" + defaultValue);
+        //logCat.info("::doEndTag - defaultValue set to [" + defaultValue + "]");
+      }
+
+      pageContext.getOut().write(tagBuf.toString());
+    }
+    catch (java.io.IOException ioe)
+    {
+      throw new JspException("IO Error: " + ioe.getMessage());
+    }
+
+    return EVAL_PAGE;
+  }
+
+
+  // ------------------------------------------------------ Protected Methods
+  // DbForms specific
+
+
+  /**
+   *  Generates the decoded name.
+   *
+   * @return  The tableFieldCode value
+   */
+  protected String getTableFieldCode()
+  {
+    StringBuffer buf  = new StringBuffer();
+
+    buf.append(parentForm.getTable().getId());
+    buf.append("_");
+    buf.append(field.getId());
+    return buf.toString();
+  }
+
+
+  /**
+   *  Gets the keyVal attribute of the DbBlobURLTag object
+   *
+   * @return  The keyVal value
+   */
+  protected String getKeyVal()
+  {
+    return parentForm.getTable().getKeyPositionString(parentForm.getResultSetVector());
+  }
 }
