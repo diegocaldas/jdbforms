4467d14d6329a8170cd49b13fd046cd60b2aa13e
foxat <>
2002-12-02 14:08:43 +0000

    pluggable event system:
    utility class for events stuff

---
+++ dbforms/src/org/dbforms/util/EventUtil.java
@@ -0,0 +1,64 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ * DbForms - a Rapid Application Development Framework
+ * Copyright (C) 2001 Joachim Peer <joepeer@excite.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+package org.dbforms.util;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.dbforms.event.*;
+
+
+
+/**
+ *  EventUtil class.
+ *
+ * @author  Luca Fossato
+ * @created  30 novembre 2002
+ */
+public class EventUtil
+{
+    /**
+     *  Gets the name of the destination table.
+     *
+     * @param  request the request object
+     * @param  action the action string
+     * @return  the destination table string
+     */
+    public final static String getDestinationTableName(HttpServletRequest request, String action)
+    {
+        return ParseUtil.getParameter(request, "data" + action + "_destTable");
+    }
+
+
+    /**
+     *  Gets the table id value.
+     *
+     * @param action the action string
+     * @return the table id value
+     */
+    public final static int getTableId(String action)
+    {
+        return ParseUtil.getEmbeddedStringAsInteger(action, 2, '_');
+    }
+}
