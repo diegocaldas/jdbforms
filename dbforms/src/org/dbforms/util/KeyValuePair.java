eb5064826ef7348d42d72d807ce92a3d94b8b9a7
foxat <>
2002-12-03 13:11:54 +0000

    added javadoc comments (not by hand...)

--- dbforms/src/org/dbforms/util/KeyValuePair.java
+++ dbforms/src/org/dbforms/util/KeyValuePair.java
@@ -23,47 +23,27 @@
 
 
 /**
-this class was introduced in DbForms 0.9
-it is used for holding simple data
-for example in EmbeddedData - Classes
-*/
+ *  this class was introduced in DbForms 0.9
+ *  it is used for holding simple data
+ *  for example in EmbeddedData - Classes
+ */
 package org.dbforms.util;
 
-/*
- * $Header$
- * $Revision$
- * $Date$
- *
- * DbForms - a Rapid Application Development Framework
- * Copyright (C) 2001 Joachim Peer <j.peer@gmx.net> et al.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- */
 
 /**
-this class was introduced in DbForms 0.9
-it is used for holding simple data
-for example in EmbeddedData - Classes
-*/
+ *  Description of the Class
+ *
+ * @author  foxat
+ * @created  3 dicembre 2002
+ */
 public class KeyValuePair
 {
     private String key;
     private String value;
 
+
     /**
-     * Creates a new KeyValuePair object.
+     *  Creates a new KeyValuePair object.
      */
     public KeyValuePair()
     {
@@ -73,8 +53,8 @@ public class KeyValuePair
     /**
      * Creates a new KeyValuePair object.
      *
-     * @param key DOCUMENT ME!
-     * @param value DOCUMENT ME!
+     * @param  key  the key value
+     * @param  value the value related to the key
      */
     public KeyValuePair(String key, String value)
     {
@@ -83,20 +63,9 @@ public class KeyValuePair
     }
 
     /**
-     * DOCUMENT ME!
+     *  Gets the key attribute of the KeyValuePair object
      *
-     * @param key DOCUMENT ME!
-     */
-    public void setKey(String key)
-    {
-        this.key = key;
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
+     * @return  The key value
      */
     public String getKey()
     {
@@ -105,23 +74,34 @@ public class KeyValuePair
 
 
     /**
-     * DOCUMENT ME!
+     *  Gets the value attribute of the KeyValuePair object
      *
-     * @param value DOCUMENT ME!
-     */
-    public void setValue(String value)
-    {
-        this.value = value;
-    }
-
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
+     * @return  The value value
      */
     public String getValue()
     {
         return value;
+    }
+
+
+    /**
+     *  Sets the key attribute of the KeyValuePair object
+     *
+     * @param  key The new key value
+     */
+    public void setKey(String key)
+    {
+        this.key = key;
+    }
+
+
+    /**
+     *  Sets the value attribute of the KeyValuePair object
+     *
+     * @param  value The new value value
+     */
+    public void setValue(String value)
+    {
+        this.value = value;
     }
 }
