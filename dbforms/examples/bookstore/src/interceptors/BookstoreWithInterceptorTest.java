7543830e04a95806c43029ebe773d9ba3485b3e1
hkollmann <>
2003-06-14 11:43:38 +0000

    added interceptor example

---
+++ dbforms/examples/bookstore/src/interceptors/BookstoreWithInterceptorTest.java
@@ -0,0 +1,66 @@
+/*
+ * Created on 13.06.2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package interceptors;
+import java.util.Hashtable;
+import java.util.Vector;
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.dbforms.event.DbEventInterceptorSupport;
+import org.dbforms.event.ValidationException;
+import org.dbforms.config.DbFormsConfig;  
+
+/**
+ * @author hkk
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class BookstoreWithInterceptorTest extends DbEventInterceptorSupport {
+	public int preInsert(HttpServletRequest request, Hashtable fieldValues,
+							  DbFormsConfig config, Connection con)
+							  throws ValidationException {
+
+		 Vector ret;
+		 Statement stmt;
+		 ResultSet rs=null;
+		 long new_id=0;
+		 String strSql="";     
+		 String strParentID="AUTHOR_ID";
+		 String strID="BOOK_ID";
+		 String strTbl="BOOK";
+		 if (fieldValues.get(strID)==null) {
+				try {
+					stmt = con.createStatement(); 
+					strSql = "select max("+strID+") from " + strTbl; 
+					if (fieldValues.get(strParentID) != null) {
+						strSql = strSql + " where " + strParentID + "=" + fieldValues.get(strParentID);
+					}
+					rs = stmt.executeQuery(strSql);  
+					rs.next();             
+					new_id = rs.getLong(1) + 1;
+			  }
+			  catch( SQLException e ) {
+					e.printStackTrace();
+			  }
+			  if (new_id==0)
+					throw new ValidationException("Error generating automatic IDs"); 
+			  else{
+					fieldValues.remove(strID);
+					fieldValues.put(strID, Long.toString(new_id));
+					fieldValues.put(strParentID, Long.toString(1));
+					return GRANT_OPERATION;          
+				}
+		  }
+		 else
+			  return GRANT_OPERATION;                
+	}
+
+
+}
