201bc28880b3e2639c6225af515bd01ebb2fd521
foxat <>
2003-07-12 10:32:26 +0000

    clean up of unused imports

--- dbforms/src/org/dbforms/event/NavEventFactoryImpl.java
+++ dbforms/src/org/dbforms/event/NavEventFactoryImpl.java
@@ -22,16 +22,15 @@
  */
 package org.dbforms.event;
 
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Category;
+//import org.apache.log4j.Category;
 
 import org.dbforms.config.Table;
 import org.dbforms.config.EventInfo;
 import org.dbforms.config.DbFormsConfig;
 import org.dbforms.event.eventtype.EventType;
-import org.dbforms.event.eventtype.EventTypeUtil;
-
 
 
 /**
