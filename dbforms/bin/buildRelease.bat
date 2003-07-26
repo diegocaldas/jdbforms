221d7a3f79b2d90e8202c60c69313d6cb6e1b2a3
hkollmann <>
2003-07-26 21:02:37 +0000

    added automatic releasing for windows.

--- dbforms/bin/buildRelease.bat
+++ dbforms/bin/buildRelease.bat
@@ -1,3 +1,18 @@
-maven dbforms:getrelease
-cd temp\release
-maven dbforms:deploy
+@echo off
+if "%SF_PRIVKEY%".==. goto NO_PRIVKEY
+if %SF_USER%.==. goto NO_USER
+call maven dbforms:getrelease
+cd temp\release\dbforms
+call maven dbforms:deploy
+cd doc
+plink -i "%SF_PRIVKEY%" %SF_USER%@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
+pscp  -i "%SF_PRIVKEY%" -r *.* %SF_USER%@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
+plink -i "%SF_PRIVKEY%" %SF_USER%@shell.sourceforge.net chmod -R g+r /home/groups/j/jd/jdbforms/htdocs
+goto ENDE
+:NO_PRIVKEY
+echo enviroment variable SF_PRIVKEY not set, set it to your SourceForge private key file first!
+goto ENDE
+:NO_USER
+echo enviroment variable SF_USER not set, set it to your SourceForge user name first!
+goto ENDE
+:ENDE
