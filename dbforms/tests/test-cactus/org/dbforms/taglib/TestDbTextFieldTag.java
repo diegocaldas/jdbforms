00a67d4212e927824d00f14370db042330d4d8d1
hkollmann <>
2003-11-03 21:05:00 +0000

    more tests

--- dbforms/tests/test-cactus/org/dbforms/taglib/TestDbTextFieldTag.java
+++ dbforms/tests/test-cactus/org/dbforms/taglib/TestDbTextFieldTag.java
@@ -24,8 +24,10 @@
 package org.dbforms.taglib;
 
 import org.apache.cactus.JspTestCase;
+import org.apache.cactus.WebRequest;
+
 import com.meterware.httpunit.WebResponse;
-import javax.servlet.http.HttpServletRequest;
+
 
 import javax.servlet.jsp.tagext.BodyTag;
 
@@ -42,22 +44,19 @@ import org.dbforms.event.DatabaseEvent;
 import org.dbforms.event.datalist.DeleteEvent;
 import org.dbforms.servlets.ConfigServlet;
 import org.dbforms.util.MessageResources;
-import org.dbforms.util.WebFormWrapper;
+import org.dbforms.util.ParseUtil;
 
 /**
- * Tests of the <code>DbLabelTag</code> class.
+ * Tests of the <code>TestDbTextFieldTag</code> class.
  *
- * @author <a href="mailto:epugh@upstate.com">Eric Pugh</a>
  *
  */
 public class TestDbTextFieldTag extends JspTestCase {
    private DbTextFieldTag doubleTag;
    private DbTextFieldTag timeTag;
    private DbFormTag form;
-   private static Date merkeDate;
-   private static Number merkeNumber;
+
    private static DbFormsConfig dbconfig;
-   private static Boolean syncHelper = new Boolean(false);
 
    public TestDbTextFieldTag(String name) throws Exception {
       super(name);
@@ -68,126 +67,101 @@ public class TestDbTextFieldTag extends JspTestCase {
     * it, this method creates a BodyContent object and passes it to the tag.
     */
    public void setUp() throws Exception {
-      synchronized (syncHelper) {
-         initConfig();
+      initConfig();
 
-         form = new DbFormTag();
-         form.setPageContext(this.pageContext);
-         form.setTableName("TIMEPLAN");
-         form.setMaxRows("*");
+      String s = ParseUtil.getParameter(request, "lang");
+      assertTrue(s != null);
+      Locale locale = new Locale(s);
+      MessageResources.setLocale(request, locale);
 
-         doubleTag = new DbTextFieldTag();
-         doubleTag.setPageContext(this.pageContext);
-         doubleTag.setParent(form);
-         doubleTag.setFieldName("D");
+      form = new DbFormTag();
+      form.setPageContext(this.pageContext);
+      form.setTableName("TIMEPLAN");
+      form.setMaxRows("*");
 
-         timeTag = new DbTextFieldTag();
-         timeTag.setPageContext(this.pageContext);
-         timeTag.setParent(form);
-         timeTag.setFieldName("TIME");
-      }
+      doubleTag = new DbTextFieldTag();
+      doubleTag.setPageContext(this.pageContext);
+      doubleTag.setParent(form);
+      doubleTag.setFieldName("D");
+
+      timeTag = new DbTextFieldTag();
+      timeTag.setPageContext(this.pageContext);
+      timeTag.setParent(form);
+      timeTag.setFieldName("TIME");
+
+      form.doStartTag();
+
+
+
+      Table table = dbconfig.getTableByName("TIMEPLAN");
+      DatabaseEvent dbEvent = new DeleteEvent(new Integer(table.getId()), "null", request, dbconfig);
+      // Set type to delete so that all fieldvalues will be parsed!!
+      dbEvent.setType(EventType.EVENT_DATABASE_DELETE);
+      FieldValues fv = dbEvent.getFieldValues();
+
+      FieldValue f = fv.get("TIME");
+      Date testDate = (Date) f.getFieldValueAsObject();
+      assertTrue(testDate instanceof java.sql.Date);
+      assertTrue(testDate.getTime() == ((Date)timeTag.getFieldObject()).getTime());
+
+    
+      f = fv.get("D");
+      Double testNumber = (Double) f.getFieldValueAsObject();
+      assertTrue(testNumber instanceof Double);
+      assertTrue(testNumber.equals(doubleTag.getFieldObject()));
+
+      int result = doubleTag.doEndTag();
+      assertEquals(BodyTag.EVAL_PAGE, result);
+
+      result = timeTag.doEndTag();
+      assertEquals(BodyTag.EVAL_PAGE, result);
+
+      form.doEndTag();
+      form.doFinally();
 
    }
 
    //-------------------------------------------------------------------------
 
-   public void testOutputDE() throws Exception {
-      synchronized (syncHelper) {
-
-         MessageResources.setLocale(request, Locale.GERMAN);
-         form.doStartTag();
-         int result = doubleTag.doEndTag();
-         assertEquals(BodyTag.EVAL_PAGE, result);
-         merkeNumber = (Number) doubleTag.getFieldObject();
-
-         result = timeTag.doEndTag();
-         assertEquals(BodyTag.EVAL_PAGE, result);
-         merkeDate = (Date) timeTag.getFieldObject();
-         form.doEndTag();
-         form.doEndTag();
-         form.doFinally();
-      }
+   public void beginOutputDE(WebRequest theRequest) throws Exception {
+      theRequest.addParameter("lang", "de");
+      theRequest.addParameter("f_8_null_1", "2,3");
+      theRequest.addParameter("of_8_null_1", "2,3");
+      theRequest.addParameter("pf_8_null_1", "#,##0.###");
+      theRequest.addParameter("f_8_null_0", "01.01.1900");
+      theRequest.addParameter("of_8_null_0", "01.01.1900");
+      theRequest.addParameter("pf_8_null_0", "dd.MM.yyyy");
    }
+
+   public void testOutputDE() throws Exception {}
 
    public void endOutputDE(WebResponse theResponse) throws Exception {
-      synchronized (syncHelper) {
-         String s = theResponse.getText();
-         boolean res = s.indexOf("value=\"2,3\"") > -1;
-         assertTrue("not found: " + "value=\"2,3\"", res);
-         res = s.indexOf("value=\"01.01.1900\"") > -1;
-         assertTrue("not found: " + "value=\"01.01.1900\"", res);
-
-         HttpServletRequest request = new WebFormWrapper(theResponse.getFormWithName("dbform"), Locale.GERMAN);
-
-         assertTrue("no config!", dbconfig != null);
-         Table table = dbconfig.getTableByName("TIMEPLAN");
-         assertTrue("no table found!", table != null);
-
-         DatabaseEvent dbEvent = new DeleteEvent(new Integer(table.getId()), "null", request, dbconfig);
-         // Set type to delete so that all fieldvalues will be parsed!!
-         dbEvent.setType(EventType.EVENT_DATABASE_DELETE);
-         FieldValues fv = dbEvent.getFieldValues();
-
-         FieldValue f = fv.get("TIME");
-         Date testDate = (Date) f.getFieldValueAsObject();
-         assertTrue(testDate instanceof java.sql.Date);
-         assertTrue(testDate.getTime() == merkeDate.getTime());
-
-         f = fv.get("D");
-         Double testNumber = (Double) f.getFieldValueAsObject();
-         assertTrue(testNumber instanceof Double);
-         assertTrue(testNumber.doubleValue() == merkeNumber.doubleValue());
-      }
+      String s = theResponse.getText();
+      boolean res = s.indexOf("value=\"2,3\"") > -1;
+      assertTrue("not found: " + "value=\"2,3\"", res);
+      res = s.indexOf("value=\"01.01.1900\"") > -1;
+      assertTrue("not found: " + "value=\"01.01.1900\"", res);
    }
 
-   public void testOutputEN() throws Exception {
-      synchronized (syncHelper) {
-
-
-         MessageResources.setLocale(request, Locale.ENGLISH);
-         form.doStartTag();
-         int result = doubleTag.doEndTag();
-         assertEquals(BodyTag.EVAL_PAGE, result);
-         merkeNumber = (Number) doubleTag.getFieldObject();
-
-         result = timeTag.doEndTag();
-         assertEquals(BodyTag.EVAL_PAGE, result);
-         merkeDate = (Date) timeTag.getFieldObject();
-         form.doEndTag();
-         form.doEndTag();
-         form.doFinally();
-      }
+   public void beginOutputEN(WebRequest theRequest) throws Exception {
+      theRequest.addParameter("lang", "en");
+      theRequest.addParameter("f_8_null_1", "2.3");
+      theRequest.addParameter("of_8_null_1", "2.3");
+      theRequest.addParameter("pf_8_null_1", "#,##0.###");
+      theRequest.addParameter("f_8_null_0", "Jan 1, 1900");
+      theRequest.addParameter("of_8_null_0", "Jan 1, 1900");
+      theRequest.addParameter("pf_8_null_0", "MMM d, yyyy");
    }
+
+
+   public void testOutputEN() throws Exception { }
 
    public void endOutputEN(WebResponse theResponse) throws Exception {
-      synchronized (syncHelper) {
-         String s = theResponse.getText();
-         boolean res = s.indexOf("value=\"2.3\"") > -1;
-         assertTrue("not found: " + "value=\"2.3\"", res);
-         res = s.indexOf("value=\"Jan 1, 1900\"") > -1;
-         assertTrue("not found : " + "value=\"Jan 1, 1900\"", res);
-
-         HttpServletRequest request = new WebFormWrapper(theResponse.getFormWithName("dbform"), Locale.ENGLISH);
-
-         assertTrue("no config!", dbconfig != null);
-         Table table = dbconfig.getTableByName("TIMEPLAN");
-         assertTrue("no table found!", table != null);
-
-         DatabaseEvent dbEvent = new DeleteEvent(new Integer(table.getId()), "null", request, dbconfig);
-         // Set type to delete so that all fieldvalues will be parsed!!
-         dbEvent.setType(EventType.EVENT_DATABASE_DELETE);
-         FieldValues fv = dbEvent.getFieldValues();
-
-         FieldValue f = fv.get("TIME");
-         Date testDate = (Date) f.getFieldValueAsObject();
-         assertTrue(testDate instanceof java.sql.Date);
-         assertTrue(testDate.getTime() == merkeDate.getTime());
-
-         f = fv.get("D");
-         Double testNumber = (Double) f.getFieldValueAsObject();
-         assertTrue(testNumber instanceof Double);
-         assertTrue(testNumber.doubleValue() == merkeNumber.doubleValue());
-      }
+      String s = theResponse.getText();
+      boolean res = s.indexOf("value=\"2.3\"") > -1;
+      assertTrue("not found: " + "value=\"2.3\"", res);
+      res = s.indexOf("value=\"Jan 1, 1900\"") > -1;
+      assertTrue("not found : " + "value=\"Jan 1, 1900\"", res);
    }
 
    private void initConfig() throws Exception {
