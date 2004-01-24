2dbfffaca82d33411a3741b77544f53937b6469b
hkollmann <>
2004-01-24 16:29:40 +0000

    change to use pageant

--- dbforms/bin/buildRelease.bat
+++ dbforms/bin/buildRelease.bat
@@ -1,18 +1,16 @@
 @echo off
-if "%SF_PRIVKEY%".==. goto NO_PRIVKEY
-if %SF_USER%.==. goto NO_USER
+if %1.==. goto NO_USER
 call maven dbforms:getrelease
 cd temp\release\dbforms
 call maven dbforms:deploy
-cd doc
-plink -i "%SF_PRIVKEY%" %SF_USER%@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
-pscp  -i "%SF_PRIVKEY%" -r *.* %SF_USER%@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
-plink -i "%SF_PRIVKEY%" %SF_USER%@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
-goto ENDE
-:NO_PRIVKEY
-echo enviroment variable SF_PRIVKEY not set, set it to your SourceForge private key file first!
+cd target\docs
+plink -i "%SF_PRIVKEY%" 	%1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
+pscp  -i "%SF_PRIVKEY%" -r *.*  %1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
+plink -i "%SF_PRIVKEY%" 	%1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
 goto ENDE
 :NO_USER
-echo enviroment variable SF_USER not set, set it to your SourceForge user name first!
+echo usage copyRelease username
+echo username is your sourceforge username!
+echo your public key must be stored in pageant!  
 goto ENDE
 :ENDE
