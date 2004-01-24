33cf8b97f03415148e9e95288b2eda5d991115ab
hkollmann <>
2004-01-24 18:09:53 +0000

    bugfix

--- dbforms/bin/buildRelease.bat
+++ dbforms/bin/buildRelease.bat
@@ -4,9 +4,9 @@ call maven dbforms:getrelease
 cd temp\release\dbforms
 call maven dbforms:deploy
 cd target\docs
-plink -i "%SF_PRIVKEY%" 	%1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
-pscp  -i "%SF_PRIVKEY%" -r *.*  %1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
-plink -i "%SF_PRIVKEY%" 	%1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
+plink 	       %1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
+pscp   -r *.*  %1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
+plink  	       %1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
 goto ENDE
 :NO_USER
 echo usage copyRelease username
--- dbforms/bin/copyRelease.bat
+++ dbforms/bin/copyRelease.bat
@@ -1,9 +1,9 @@
 @echo off
 if %1.==. goto NO_USER
 cd target\docs
-plink -i 	 	%1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
-pscp  -i  -r *.* 	%1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
-plink -i  		%1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
+plink 	 	%1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
+pscp   -r *.* 	%1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
+plink  		%1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
 goto ENDE
 :NO_USER
 echo usage copyRelease username
