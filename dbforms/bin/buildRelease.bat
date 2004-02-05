24582acceaeb8c0e32a104ec0a47e69c34926c02
hkollmann <>
2004-02-05 17:34:08 +0000

    changed paths

--- dbforms/bin/buildRelease.bat
+++ dbforms/bin/buildRelease.bat
@@ -1,15 +1,15 @@
 @echo off
 if %1.==. goto NO_USER
 call maven dbforms:getrelease
-cd temp\release\dbforms
+cd target\release\dbforms
 call maven dbforms:deploy
 cd target\docs
 plink 	       %1@shell.sourceforge.net rm -f /home/groups/j/jd/jdbforms/htdocs/weekly/*
-pscp   -r *.*  %1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
+pscp   -r *.*        %1@shell.sourceforge.net:/home/groups/j/jd/jdbforms/htdocs
 plink  	       %1@shell.sourceforge.net chmod -R g+w /home/groups/j/jd/jdbforms/htdocs
 goto ENDE
 :NO_USER
-echo usage copyRelease username
+echo usage buildRelease username
 echo username is your sourceforge username!
 echo your public key must be stored in pageant!  
 goto ENDE
