b965f458827f0939c128c98560ee20ca3a63118f
hkollmann <>
2004-03-04 17:24:02 +0000

    replaceRealPath will replace all occurence now

--- dbforms/src/org/dbforms/util/Util.java
+++ dbforms/src/org/dbforms/util/Util.java
@@ -33,169 +33,164 @@ import java.io.UnsupportedEncodingException;
  */
 public class Util {
 
-    private static final String REALPATH = "$(SERVLETCONTEXT_REALPATH)";
+   private static final String REALPATH = "$(SERVLETCONTEXT_REALPATH)";
 
-    /**
-     * Test if the input string is null or empty (does not contain any
-     * character)
-     * 
-     * @param s
-     *            the string value to test
-     * 
-     * @return true if the input string is null or empty, false otherwise
-     */
-    public static final boolean isNull(String s) {
-        return ((s == null) || (s.trim().length() == 0));
-    }
+   /**
+    * Test if the input string is null or empty (does not contain any
+    * character)
+    * 
+    * @param s
+    *            the string value to test
+    * 
+    * @return true if the input string is null or empty, false otherwise
+    */
+   public static final boolean isNull(String s) {
+      return ((s == null) || (s.trim().length() == 0));
+   }
 
-    /**
-     * Replaces the occurens from REALPATH in s with realpath.
-     * 
-     * @param s
-     *            the string containing the REALPATH token
-     * @param realpath
-     *            the value used to replace the REALPATH token
-     * 
-     * @return the input string, with the REALPATH token replaced with the
-     *         realpath value
-     */
-    public static final String replaceRealPath(String s, String realpath) {
-        if (!isNull(realpath)) {
-            // 20030604-HKK: Bugfixing for different engine, e.g. cactus. Path
-            // maybe without trailing '/'!!!
-            if (realpath.charAt(realpath.length() - 1) != '/') {
-                realpath = realpath + '/';
-            }
+   /**
+    * Replaces the occurens from REALPATH in s with realpath.
+    * 
+    * @param s
+    *            the string containing the REALPATH token
+    * @param realpath
+    *            the value used to replace the REALPATH token
+    * 
+    * @return the input string, with the REALPATH token replaced with the
+    *         realpath value
+    */
+   public static final String replaceRealPath(String s, String realpath) {
+      if (!isNull(realpath)) {
+         // 20030604-HKK: Bugfixing for different engine, e.g. cactus. Path
+         // maybe without trailing '/'!!!
+         if (realpath.charAt(realpath.length() - 1) != '/') {
+            realpath = realpath + '/';
+         }
+         int i = s.indexOf(REALPATH);
+         while (i >= 0) {
+            StringBuffer buf = new StringBuffer();
+            buf.append(s.substring(0, i));
+            buf.append(realpath);
+            buf.append(s.substring(i + REALPATH.length() + 1));
+            s = buf.toString();
+            i = s.indexOf(REALPATH);
+         }
+      }
 
-            int i = s.indexOf(REALPATH);
+      return s;
+   }
 
-            if (i >= 0) {
-                StringBuffer buf = new StringBuffer();
-                buf.append(s.substring(0, i));
-                buf.append(realpath);
-                buf.append(s.substring(i + REALPATH.length() + 1));
-                s = buf.toString();
-            }
-        }
+   /**
+    * Encode a string with desired character encoding.
+    * 
+    * @param s
+    *            the string to encode
+    * @param enc
+    *            the desired encoding
+    * 
+    * @return the encoded string
+    * @throws UnsupportedEncodingException
+    *             DOCUMENT ME!
+    */
+   public static final String encode(String s, String enc) throws UnsupportedEncodingException {
+      if (!Util.isNull(s)) {
+         try {
+            s = encCheck(s, enc);
+         } catch (NoSuchMethodException nsme) {
+            s = URLEncoder.encode(s);
+         }
+      }
 
-        return s;
-    }
+      return s;
+   }
 
-    /**
-     * Encode a string with desired character encoding.
-     * 
-     * @param s
-     *            the string to encode
-     * @param enc
-     *            the desired encoding
-     * 
-     * @return the encoded string
-     * @throws UnsupportedEncodingException
-     *             DOCUMENT ME!
-     */
-    public static final String encode(String s, String enc)
-            throws UnsupportedEncodingException {
-        if (!Util.isNull(s)) {
-            try {
-                s = encCheck(s, enc);
-            } catch (NoSuchMethodException nsme) {
-                s = URLEncoder.encode(s);
-            }
-        }
+   /**
+    * Decodes a string
+    * 
+    * @param s
+    *            the string to encode
+    * 
+    * @return the encoded string
+    */
+   public static final String decode(String s, String enc) throws UnsupportedEncodingException {
+      if (!Util.isNull(s)) {
+         try {
+            s = decCheck(s, enc);
+         } catch (NoSuchMethodException nsme) {
+            s = URLDecoder.decode(s);
+         }
+      }
 
-        return s;
-    }
+      return s;
+   }
 
-    /**
-     * Decodes a string
-     * 
-     * @param s
-     *            the string to encode
-     * 
-     * @return the encoded string
-     */
-    public static final String decode(String s, String enc)
-            throws UnsupportedEncodingException {
-        if (!Util.isNull(s)) {
-            try {
-                s = decCheck(s, enc);
-            } catch (NoSuchMethodException nsme) {
-                s = URLDecoder.decode(s);
-            }
-        }
+   /**
+    * returns a formated string
+    * 
+    * @param format
+    *            format string
+    * @param o
+    *            objects to use to format
+    * 
+    * @return String
+    */
+   public static String sprintf(String format, Object[] o) {
+      PrintfFormat printfFormat = new PrintfFormat(format); // create
+      // instance of
+      // PrintfFormat
+      // class
 
-        return s;
-    }
+      return printfFormat.sprintf(o);
+   }
 
-    /**
-     * returns a formated string
-     * 
-     * @param format
-     *            format string
-     * @param o
-     *            objects to use to format
-     * 
-     * @return String
-     */
-    public static String sprintf(String format, Object[] o) {
-        PrintfFormat printfFormat = new PrintfFormat(format); // create
-                                                              // instance of
-                                                              // PrintfFormat
-                                                              // class
+   /**
+    * needed by Util.encode(s,enc) to check if URLEncoder.encode(String s,
+    * String enc) is available (from jdk 1.4)
+    * 
+    * @param s
+    *            the string to encode
+    * @param enc
+    *            the desired encoding
+    * 
+    * @return the encoded string
+    * 
+    * @throws UnsupportedEncodingException
+    *             DOCUMENT ME!
+    * @throws NoSuchMethodException
+    *             to signal that jdk 1.3 is being used
+    */
+   private static final String decCheck(String s, String enc) throws UnsupportedEncodingException, NoSuchMethodException {
+      if (isNull(enc)) {
+         enc = "UTF-8";
+      }
+      return URLDecoder.decode(s, enc);
+   }
 
-        return printfFormat.sprintf(o);
-    }
+   /**
+    * needed by Util.encode(s,enc) to check if URLEncoder.encode(String s,
+    * String enc) is available (from jdk 1.4)
+    * 
+    * @param s
+    *            the string to encode
+    * @param enc
+    *            the desired encoding
+    * 
+    * @return the encoded string
+    * 
+    * @throws UnsupportedEncodingException
+    *             DOCUMENT ME!
+    * @throws NoSuchMethodException
+    *             to signal that jdk 1.3 is being used
+    */
+   private static final String encCheck(String s, String enc) throws UnsupportedEncodingException, NoSuchMethodException {
+      if (isNull(enc)) {
+         enc = "UTF-8";
+      }
 
-    /**
-     * needed by Util.encode(s,enc) to check if URLEncoder.encode(String s,
-     * String enc) is available (from jdk 1.4)
-     * 
-     * @param s
-     *            the string to encode
-     * @param enc
-     *            the desired encoding
-     * 
-     * @return the encoded string
-     * 
-     * @throws UnsupportedEncodingException
-     *             DOCUMENT ME!
-     * @throws NoSuchMethodException
-     *             to signal that jdk 1.3 is being used
-     */
-    private static final String decCheck(String s, String enc)
-            throws UnsupportedEncodingException, NoSuchMethodException {
-        if (isNull(enc)) {
-            enc = "UTF-8";
-        }
-        return URLDecoder.decode(s, enc);
-    }
+      s = URLEncoder.encode(s, enc);
 
-    /**
-     * needed by Util.encode(s,enc) to check if URLEncoder.encode(String s,
-     * String enc) is available (from jdk 1.4)
-     * 
-     * @param s
-     *            the string to encode
-     * @param enc
-     *            the desired encoding
-     * 
-     * @return the encoded string
-     * 
-     * @throws UnsupportedEncodingException
-     *             DOCUMENT ME!
-     * @throws NoSuchMethodException
-     *             to signal that jdk 1.3 is being used
-     */
-    private static final String encCheck(String s, String enc)
-            throws UnsupportedEncodingException, NoSuchMethodException {
-        if (isNull(enc)) {
-            enc = "UTF-8";
-        }
+      return s;
 
-        s = URLEncoder.encode(s, enc);
-
-        return s;
-
-    }
+   }
 
 }
