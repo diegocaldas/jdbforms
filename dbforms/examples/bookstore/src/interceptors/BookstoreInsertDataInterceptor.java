599c5bb46c5bd2b4bdfae6a38414560ee269bd93
hkollmann <>
2004-03-27 20:26:29 +0000

    no message

---
+++ dbforms/examples/bookstore/src/interceptors/BookstoreInsertDataInterceptor.java
@@ -0,0 +1,41 @@
+package interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.dbforms.event.DbEventInterceptorSupport;
+import org.dbforms.config.DbFormsConfig;
+import org.dbforms.config.ValidationException;
+import org.dbforms.config.Table;
+import org.dbforms.config.FieldValues;
+
+/**
+ * @author hkk
+ *
+ * Folgendes auswählen, um die Schablone für den erstellten Typenkommentar zu ändern:
+ * Fenster&gt;Benutzervorgaben&gt;Java&gt;Codegenerierung&gt;Code und Kommentare
+ */
+public class BookstoreInsertDataInterceptor extends DbEventInterceptorSupport {
+
+   public int preInsert(HttpServletRequest request, Table table, FieldValues fieldValues, DbFormsConfig config, Connection con)
+      throws ValidationException {
+      long new_id = 0;
+      String fieldName = table.getName() + "_ID";
+      String qry = "select max(" + fieldName  + ") from " + table.getName();
+      try {
+         Statement stmt = con.createStatement();
+         ResultSet rs = stmt.executeQuery(qry);
+         rs.next();
+         new_id = rs.getLong(1);
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      new_id++;
+      setValue(table, fieldValues, fieldName, String.valueOf(new_id));
+      return GRANT_OPERATION;
+   }
+
+}
