759afdac2e4701aebdb757dc3d6bc23b1e13c330
nealkatz <>
2004-08-02 06:14:35 +0000

    Initial version.  Datsource for reports that is based on iteratoring over a container, instead of using a dbforms result set

---
+++ dbforms/src/org/dbforms/servlets/reports/JRDataSourceIter.java
@@ -0,0 +1,233 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ * DbForms - a Rapid Application Development Framework
+ * Copyright (C) 2001 Joachim Peer <joepeer@excite.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+
+/**
+ * Use an iterator as data source
+ * 
+ * HOWTO
+ * 1) create a Collection of X. Where X is a complex object with 'fields', X can either be
+ * a Set or a Bean. The container must support Iterator(). Object X must be supported 
+ * by apache bean utils
+ * 2) somewhere in pagecontext (like session) put the Collection with key "jasper.input"
+ * 3) invoke
+ * 
+ * gets field data from multiple sources, not just DBform fields
+ * context : prefix context__xxx  . Uses pageContext.findAttribute(xxx)
+ * session : prefix session__xxxx . Uses  pageContext.getSession().findAttribute(xxx)
+ * internal : prefix internal_zzz
+ * attribute of iterator object : no prefix  
+ * 
+ * For internal, only internal__rownum is currently defined
+ * For content and session, xxxx may be a 'simple' type or 'complex' type
+ * a 'complex' type is an object supporting javabean access, instead of the
+ * usual '.' notation, use '__' instead.
+ * 
+ * @author Neal Katz
+ */
+
+
+package org.dbforms.servlets.reports;
+
+/**
+ */
+import java.util.Iterator;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.PageContext;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.log4j.Category;
+import org.dbforms.util.IGetSession;
+
+import dori.jasper.engine.JRDataSource;
+import dori.jasper.engine.JRException;
+import dori.jasper.engine.JRField;
+
+/**
+ * DOCUMENT ME!
+ * 
+ * @version $Revision$
+ * @author $author$
+ */
+public class JRDataSourceIter implements JRDataSource, IGetSession {
+	private int rownum=0;
+	private static Category logCat =
+		Category.getInstance(JRDataSourceIter.class.getName());
+	private PageContext pageContext;
+	private Iterator iter;
+	Object current = null;
+
+	/**
+		* @param iter
+		* @param pageContext
+		*/
+	public JRDataSourceIter(Iterator iter, PageContext pageContext) {
+		this.pageContext = pageContext;
+		this.iter = iter;
+		rownum=0;
+	}
+
+	/**
+		* @see dori.jasper.engine.JRDataSource#getFieldValue(dori.jasper.engine.JRField)
+		* 
+		*/
+	public Object getFieldValue(JRField field) throws JRException {
+		Object o;
+		
+		String search = field.getName();
+
+		if (search.startsWith("context__")) {
+			logCat.debug(
+				"Trying to find data for Page Context value: " + search);
+			o = getPageContextValue(search);
+		} else if (search.startsWith("internal__")) {
+			logCat.debug("Trying to find data for session value: " + search);
+			o = getInternalValue(search);
+		} else if (search.startsWith("session__")) {
+			logCat.debug("Trying to find data for session value: " + search);
+			o = getSessionValue(search);
+		} else {
+			logCat.debug("Trying to find data for field named: " + search);
+			o = getCurrentValue(search);
+		}
+
+		return o;
+	}
+
+	/**
+	 * @param search
+	 * @return
+	 */
+	private Object getInternalValue(String search) {
+		Object obj = null;
+		int pos;
+		try {
+			search = search.substring(search.indexOf("__") + 2);
+			logCat.debug("Trying to find data for internal var : " + search);
+			if (search.equalsIgnoreCase("rownum")) {
+				return new Integer(rownum);
+			}
+		} catch (Exception e) {
+			logCat.error("getPageContextValue: " + e);
+		}
+		return obj;
+	}
+
+	/**
+	* @param search
+	* @return
+	*/
+	private Object getPageContextValue(String search) {
+		Object obj = null;
+		int pos;
+		try {
+			search = search.substring(search.indexOf("__") + 2);
+			logCat.debug("Trying to find data for page context var : " + search);
+			pos = search.indexOf("__");
+			if (pos == -1) {
+				// simple type, 'search' is an object in the session
+				obj = pageContext.findAttribute(search);
+			} else {
+				// complex, 'search' is really a bean
+				String[] sa = search.split("__", 2);
+				Object bean = pageContext.findAttribute(sa[0]);
+				sa[1] = sa[1].replaceAll("__", ".");
+				logCat.debug(
+					"calling PropertyUtils.getProperty " + sa[0] + " " + sa[1]);
+				obj = PropertyUtils.getProperty(bean, sa[1]);
+			}
+		} catch (Exception e) {
+			logCat.error("getPageContextValue: " + e);
+		}
+		return obj;
+	}
+
+	/**
+	* @param search
+	* @return
+	*/
+	private Object getSessionValue(String search) {
+		Object obj = null;
+		int pos;
+		try {
+			HttpSession session = this.pageContext.getSession();
+			search = search.substring(search.indexOf("__") + 2);
+			logCat.debug("Trying to find data for session var : " + search);
+			pos = search.indexOf("__");
+			if (pos == -1) {
+				// simple type, 'search' is an object in the session
+				obj = session.getAttribute(search);
+			} else {
+				// complex, 'search' is really a bean
+				String[] sa = search.split("__", 2);
+				Object bean = session.getAttribute(sa[0]);
+				sa[1].replaceAll("__", ".");
+				logCat.debug(
+					"calling PropertyUtils.getProperty " + sa[0] + " " + sa[1]);
+				obj = PropertyUtils.getProperty(bean, sa[1]);
+			}
+		} catch (Exception e) {
+			logCat.error("getSessionValue: " + e);
+		}
+		return obj;
+	}
+	/**
+	* @param search
+	* @return
+	*/
+	private Object getCurrentValue(String search) {
+		Object obj = null;
+		int pos;
+		try {
+			if (current != null) {
+				// complex, current is a container or a bean
+				String sa = search;
+				sa.replaceAll("__", ".");
+				logCat.debug(
+					"calling PropertyUtils.getProperty " + current + " " + sa);
+				obj = PropertyUtils.getProperty(current, sa);
+			}
+		} catch (Exception e) {
+			logCat.error("getCurrentValue: " + e);
+		}
+		return obj;
+	}
+
+	/**
+		* @see dori.jasper.engine.JRDataSource#next()
+		*/
+	public boolean next() throws JRException {
+		if (!iter.hasNext())
+			return false;
+		current = iter.next();
+		rownum++;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.dbforms.servlets.reports.IGetSession#getSession()
+	 */
+	public HttpSession getSession() {
+		return pageContext.getSession();
+	}
+}
