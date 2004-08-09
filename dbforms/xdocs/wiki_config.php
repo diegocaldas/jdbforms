4a1cbd8fe9cfce9ad0db39771fe200b10f482941
hkollmann <>
2004-08-09 17:56:09 +0000

    minor changes for the wiki

--- dbforms/xdocs/wiki_config.php
+++ dbforms/xdocs/wiki_config.php
@@ -6,7 +6,7 @@ chdir(dirname(__FILE__)."/ewiki");
 #-- database connection/plugins
 // MySQL support is built-in, we only open the connection
 define("EWIKI_DB_TABLE_NAME", "ewiki");
-define("EWIKI_SCRIPT", "wiki.php?page=");
+define("EWIKI_SCRIPT", "http://jdbforms.sourceforge.net/wiki.php?page=");
 
 
 #-- OPEN DATABASE for ewiki
@@ -16,18 +16,18 @@ if (function_exists("mysql_connect")) {
 }
 
 #-- or use the flat file database backend as fallback
-if (!$ok) {
-   // define("EWIKI_DBFILES_DIRECTORY", "/tmp");
-   include("plugins/db/flat_files.php");
-}
+#if (!$ok) {
+#   // define("EWIKI_DBFILES_DIRECTORY", "/tmp");
+#   include("plugins/db/flat_files.php");
+#}
 
 
 #-- constants
 define("EWIKI_NAME", 'DbFormsWiki');
-define("EWIKI_PAGE_INDEX", 'FrontPage');
-define("EWIKI_PAGE_UPDATES", 'UpdatedPages');
-define("EWIKI_PAGE_NEWEST", 'NewestPages');
-define("EWIKI_PAGE_SEARCH", 'SearchPages');
+define("EWIKI_PAGE_INDEX",       'WiKi');
+define("EWIKI_PAGE_UPDATES",     'UpdatedPages');
+define("EWIKI_PAGE_NEWEST",      'NewestPages');
+define("EWIKI_PAGE_SEARCH",      'SearchPages');
 define("EWIKI_PAGE_POWERSEARCH", 'PowerSearch');
 define("EWIKI_SCRIPT", '?id=');
 define("EWIKI_AUTO_EDIT", 1);
@@ -38,8 +38,10 @@ define("EWIKI_HTTP_HEADERS", 0);
 define("EWIKI_ENGAGE_BINARY", 1);
 define("EWIKI_CACHE_IMAGES", 1);
 define("EWIKI_IMAGE_MAXSIZE", 65536);
-define("EWIKI_LOGFILE", '/tmp/ewiki.log');
-define("EWIKI_LOGLEVEL", '-1');
+define("EWIKI_LOGFILE", '/home/groups/j/jd/jdbforms/htdocs/log/ewiki.txt');
+define("EWIKI_LOGLEVEL", '3');
+define("EWIKI_TMP", '/home/groups/j/jd/jdbforms/tmp');
+
 define("EWIKI_CASE_INSENSITIVE", 1);
 define("EWIKI_PRINT_TITLE", 1);
 define("EWIKI_CONTROL_LINE", 1);
@@ -63,10 +65,10 @@ include("plugins/notify.php");
 include("plugins/patchsaving.php");
 
 include("plugins/feature/appendonly.php");
-include("plugins/feature/imgresize_gd.php");
+#include("plugins/feature/imgresize_gd.php");
 include("plugins/feature/imgfile_naming.php");
 
-include("plugins/mpi/mpi.php");
+#include("plugins/mpi/mpi.php");
 
 include("plugins/linking/link_css.php");
 include("plugins/linking/linkexcerpts.php");
@@ -74,10 +76,10 @@ include("plugins/linking/linkexcerpts.php");
 include("plugins/page/powersearch.php");
 include("plugins/page/pageindex.php");
 include("plugins/page/wordindex.php");
-include("plugins/page/orphanedpages.php");
-include("plugins/page/wantedpages.php");
-include("plugins/page/hitcounter.php");
-include("plugins/page/textupload.php");
+#include("plugins/page/orphanedpages.php");
+#include("plugins/page/wantedpages.php");
+#include("plugins/page/hitcounter.php");
+#include("plugins/page/textupload.php");
 include("plugins/page/wikinews.php");
 #include("plugins/page/wikiuserlogin.php");
 
@@ -89,8 +91,8 @@ include("plugins/module/downloads.php");
 include("plugins/markup/css.php");
 include("plugins/markup/naturallists.php");
 
-include("fragments/strip_wonderful_slashes.php");
-include("fragments/strike_register_globals.php");
+#include("fragments/strip_wonderful_slashes.php");
+#include("fragments/strike_register_globals.php");
 
 include("plugins/filter/search_highlight.php");
 
@@ -99,12 +101,12 @@ include("plugins/filter/search_highlight.php");
 
 define("EWIKI_PROTECTED_MODE", 1);
 #define("EWIKI_PROTECTED_MODE_HIDING", 1);
-define("EWIKI_AUTO_LOGIN",1);
+define("EWIKI_AUTO_LOGIN", 1);
 include("plugins/auth/auth_perm_ring.php");
 include("plugins/auth/userdb_array.php");
 
-include("wiki_auth_method_form.php");
-include("wiki_userdb_userregistry.php");
+include("../wiki_auth_method_form.php");
+include("../wiki_userdb_userregistry.php");
 
 include("plugins/admin/control.php");
 
@@ -122,7 +124,6 @@ $ewiki_t["languages"][] = "en";
 
 # admin account 
 $ewiki_auth_user_array = array(
-
    "admin"  => array("jdbforms", $RING_LEVEL=0)
 );
 
