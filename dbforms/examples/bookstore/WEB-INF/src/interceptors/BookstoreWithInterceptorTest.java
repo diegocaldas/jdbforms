3828371a01e9cb7d8d6d3d762d1c02a08f4e2e9a
hkollmann <>
2004-11-14 20:18:46 +0000

    testing of javascript vars.

--- dbforms/examples/bookstore/WEB-INF/src/interceptors/BookstoreWithInterceptorTest.java
+++ dbforms/examples/bookstore/WEB-INF/src/interceptors/BookstoreWithInterceptorTest.java
@@ -21,8 +21,11 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 package interceptors;
-import org.apache.log4j.Category;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.dbforms.config.ConfigLoader;
 import org.dbforms.config.DbFormsConfig;
 import org.dbforms.config.FieldValues;
 import org.dbforms.config.Table;
@@ -37,118 +40,130 @@ import java.sql.Statement;
 
 import javax.servlet.http.HttpServletRequest;
 
-
-
 /**
  * @author hkk
- *
+ * 
  * To change this generated comment go to Window>Preferences>Java>Code
  * Generation>Code and Comments
  */
 public class BookstoreWithInterceptorTest extends DbEventInterceptorSupport {
-   private Category logCat = Category.getInstance(this.getClass().getName());
+	private static Log logCat = LogFactory.getLog(ConfigLoader.class);
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param request DOCUMENT ME!
-    * @param table DOCUMENT ME!
-    * @param fieldValues DOCUMENT ME!
-    * @param config DOCUMENT ME!
-    * @param con DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    *
-    * @throws ValidationException DOCUMENT ME!
-    */
-   public int preInsert(HttpServletRequest request, Table table,
-      FieldValues fieldValues, DbFormsConfig config, Connection con)
-      throws ValidationException {
-      logCat.info("preInsert called");
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param request
+	 *            DOCUMENT ME!
+	 * @param table
+	 *            DOCUMENT ME!
+	 * @param fieldValues
+	 *            DOCUMENT ME!
+	 * @param config
+	 *            DOCUMENT ME!
+	 * @param con
+	 *            DOCUMENT ME!
+	 * 
+	 * @return DOCUMENT ME!
+	 * 
+	 * @throws ValidationException
+	 *             DOCUMENT ME!
+	 */
+	public int preInsert(HttpServletRequest request, Table table,
+			FieldValues fieldValues, DbFormsConfig config, Connection con)
+			throws ValidationException {
+		logCat.info("preInsert called");
 
-      Statement stmt;
-      ResultSet rs          = null;
-      long      new_id      = 0;
-      String    strSql      = "";
-      String    strParentID = "AUTHOR_ID";
-      String    strID       = "BOOK_ID";
-      String    strTbl      = "BOOK";
+		Statement stmt;
+		ResultSet rs = null;
+		long new_id = 0;
+		String strSql = "";
+		String strParentID = "AUTHOR_ID";
+		String strID = "BOOK_ID";
+		String strTbl = "BOOK";
 
-      if (fieldValues.get(strID) == null) {
-         try {
-            stmt      = con.createStatement();
-            strSql    = "select max(" + strID + ") from " + strTbl;
+		if (fieldValues.get(strID) == null) {
+			try {
+				stmt = con.createStatement();
+				strSql = "select max(" + strID + ") from " + strTbl;
 
-            if (fieldValues.get(strParentID) != null) {
-               strSql = strSql + " where " + strParentID + "="
-                  + fieldValues.get(strParentID);
-            }
+				if (fieldValues.get(strParentID) != null) {
+					strSql = strSql + " where " + strParentID + "="
+							+ fieldValues.get(strParentID);
+				}
 
-            rs = stmt.executeQuery(strSql);
-            rs.next();
-            new_id = rs.getLong(1) + 1;
-            stmt.close();
-         } catch (SQLException e) {
-            e.printStackTrace();
-         }
+				rs = stmt.executeQuery(strSql);
+				rs.next();
+				new_id = rs.getLong(1) + 1;
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
-         if (new_id == 0) {
-            throw new ValidationException("Error generating automatic IDs");
-         }
+			if (new_id == 0) {
+				throw new ValidationException("Error generating automatic IDs");
+			}
 
-         fieldValues.remove(strID);
-         setValue(table, fieldValues, strID, Long.toString(new_id));
-         setValue(table, fieldValues, strParentID, Long.toString(1));
+			fieldValues.remove(strID);
+			setValue(table, fieldValues, strID, Long.toString(new_id));
+			setValue(table, fieldValues, strParentID, Long.toString(1));
 
-         // Test: set title to fixed string!
-         setValue(table, fieldValues, "TITLE", "fixed title in new interceptor");
-      }
+			// Test: set title to fixed string!
+			setValue(table, fieldValues, "TITLE",
+					"fixed title in new interceptor");
+		}
 
-      return GRANT_OPERATION;
-   }
+		return GRANT_OPERATION;
+	}
 
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param request
+	 *            DOCUMENT ME!
+	 * @param config
+	 *            DOCUMENT ME!
+	 * @param con
+	 *            DOCUMENT ME!
+	 * 
+	 * @return DOCUMENT ME!
+	 */
+	public int preSelect(HttpServletRequest request, DbFormsConfig config,
+			Connection con) {
+		logCat.info("preSelect called");
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param request DOCUMENT ME!
-    * @param config DOCUMENT ME!
-    * @param con DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   public int preSelect(HttpServletRequest request, DbFormsConfig config,
-      Connection con) {
-      logCat.info("preSelect called");
+		return GRANT_OPERATION;
+	}
 
-      return GRANT_OPERATION;
-   }
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param request
+	 *            DOCUMENT ME!
+	 * @param table
+	 *            DOCUMENT ME!
+	 * @param fieldValues
+	 *            DOCUMENT ME!
+	 * @param config
+	 *            DOCUMENT ME!
+	 * @param con
+	 *            DOCUMENT ME!
+	 * 
+	 * @return DOCUMENT ME!
+	 * 
+	 * @throws ValidationException
+	 *             DOCUMENT ME!
+	 */
+	public int preUpdate(HttpServletRequest request, Table table,
+			FieldValues fieldValues, DbFormsConfig config, Connection con)
+			throws ValidationException {
+		logCat.info("preUpdate called");
 
+		if ("42".equals(fieldValues.get("ISBN").getFieldValue())) {
+			return IGNORE_OPERATION;
+		}
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param request DOCUMENT ME!
-    * @param table DOCUMENT ME!
-    * @param fieldValues DOCUMENT ME!
-    * @param config DOCUMENT ME!
-    * @param con DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    *
-    * @throws ValidationException DOCUMENT ME!
-    */
-   public int preUpdate(HttpServletRequest request, Table table,
-      FieldValues fieldValues, DbFormsConfig config, Connection con)
-      throws ValidationException {
-      logCat.info("preUpdate called");
+		fieldValues.remove("ISBN");
 
-      if ("42".equals(fieldValues.get("ISBN").getFieldValue())) {
-         return IGNORE_OPERATION;
-      }
-
-      fieldValues.remove("ISBN");
-
-      return GRANT_OPERATION;
-   }
+		return GRANT_OPERATION;
+	}
 }
---
+++ dbforms/examples/bookstore/tests/testAuthorBooksSubFormWithJavascriptFieldsArray.jsp
@@ -0,0 +1,145 @@
+<html>
+	<%@ taglib uri="/WEB-INF/dbforms.tld" prefix="db" %>
+	<head>
+	   <db:base />
+	   <target="_top">
+	</head>
+   <body>
+      <db:dbform autoUpdate="false" 
+                 followUp="/tests/testAuthorBooksSubFormWithJavascriptFieldsArray.jsp" 
+                 maxRows="1" 
+                 tableName="AUTHOR" 
+				 javascriptFieldsArray="true"
+                 
+      >
+         <db:header>
+	         <h1 align="center">Edit Authors</h1>
+	         <db:errors/>
+         </db:header>
+         <db:body>
+            <table class="fixed" align="center">
+               <tr class="even">
+                     <td style="width:300px">ID</td>
+			         <td style="width:100px"><db:label fieldName="AUTHOR_ID"/>&nbsp;</td>
+               </tr>
+               <tr class="odd">
+                    <td style="width:300px">NAME</td>
+                    <td style="width:300px"><db:textField size="25" fieldName="NAME"/></td>
+               </tr>
+               <tr class="even">
+                    <td>ORGANISATION</td>
+                    <td><db:textField size="25" fieldName="ORGANISATION"/>
+                    </td>
+               </tr>
+            <tr class="button">
+               <td colspan="2" style="text-align:center">
+                 <db:updateButton style="width:100" caption="Save"/>
+                 <db:deleteButton style="width:100" caption="Delete"/>
+                 <db:insertButton style="width:100" caption="Insert" showAlways="false" />
+               </td>
+            </tr>
+            <tr class="button">
+               <td colspan="2" style="text-align:center">
+                 <db:navFirstButton style="width:100" caption="<< First"/>
+                 <db:navPrevButton  style="width:100" caption="<  Previous"/>
+                 <db:navNextButton  style="width:100" caption=">  Next"/>
+                 <db:navLastButton  style="width:100" caption=">> Last"/>
+                 <db:navNewButton   style="width:100" caption="New"  showAlwaysInFooter="false"/>
+                 <db:navCopyButton  style="width:100" caption="Copy" showAlwaysInFooter="false"/>
+                 &nbsp;
+               </td>
+            </tr>
+		    <tr>
+				<td colspan="2" style="text-align:center">
+					<h2>sub form</h2>
+				</td>
+			</tr>
+		    <tr><td colspan="2" style="text-align:center">
+<% 
+	int iPos = 0;
+%>			
+			    <db:dbform 
+		      		autoUpdate="false" 
+		      		followUp="/tests/testAuthorBooksSubFormWithJavascriptFieldsArray.jsp"   
+		      		maxRows="*" 
+		      		tableName="BOOK"
+		     		parentField="AUTHOR_ID"
+		     		childField="AUTHOR_ID"	 			
+			      	orderBy="ISBN" 
+					javascriptFieldsArray="true" 
+			    >
+		         <db:header>
+					<table>
+				 </db:header>
+		         <db:body allowNew="true">
+    	           <tr>
+        	          <td><db:label fieldName="BOOK_ID"/>&nbsp;</td>
+<% 
+	String strGetDbFormFieldName = "";
+	String strOnFocus = "";
+	strGetDbFormFieldName = "getDbFormFieldName('ISBN', ";
+	if (currentRow_BOOK == null) {
+		strGetDbFormFieldName += "null";
+	}
+	else{
+		strGetDbFormFieldName += iPos;
+	}
+	strGetDbFormFieldName += ")";
+	strOnFocus  = "javascript:";
+	strOnFocus += "if (" +  strGetDbFormFieldName + " == this.name){";
+	strOnFocus +=     "alert('OK! Vars are equal in name:\\n\\n' + ";
+	strOnFocus +=     "'getDbFormFieldName=' + " + strGetDbFormFieldName +" + '\\n' + ";
+	strOnFocus +=     "'this.name=' + this.name);";
+	strOnFocus += "}";
+	strOnFocus += "else{";
+	strOnFocus +=     "alert('ERROR! Vars differ in name:\\n\\n' + ";
+	strOnFocus +=     "'getDbFormFieldName=' + " + strGetDbFormFieldName +" + '\\n' + ";
+	strOnFocus +=     "'this.name=' + this.name);";
+	strOnFocus += "}";
+	
+
+%>
+            	      <td>
+						<db:textField fieldName="ISBN" overrideValue="Click here to check!" onClick="<%=strOnFocus%>"/>
+					  	<db:textField fieldName="ISBN"/>&nbsp;
+					  </td>
+                	  <td><db:textField fieldName="TITLE" /> </td>
+                	  <td>
+							 <db:updateButton style="width:100" caption="Save"/>
+							 <db:deleteButton style="width:100" caption="Delete"/>
+							 <db:insertButton style="width:100" caption="Insert" showAlways="false" />
+					  </td>
+	               </tr>			              		 				
+<% iPos++; %>							   
+				 </db:body>
+		         <db:footer>
+					  <tr class="button">
+					   	<td colspan="4" style="text-align:center">
+						 	<db:navFirstButton style="width:100" caption="<< First"/>
+					   		<db:navPrevButton  style="width:100" caption="<  Previous"/>
+							<db:navNextButton  style="width:100" caption=">  Next"/>
+						 	<db:navLastButton  style="width:100" caption=">> Last"/>
+						 	<db:navNewButton   style="width:100" caption="New"/>
+		                 	<db:navCopyButton  style="width:100" caption="Copy"/>
+						 	&nbsp;
+					   	</td>
+					  </tr>
+					<table>
+		         </db:footer>
+				</db:dbform>	
+			</td></tr>
+         </db:body>
+         <db:footer>
+           </table>
+         </db:footer>
+      </db:dbform>
+
+<%@ include file="httpSnooper.jsp" %> 
+
+   </body>
+</html>
+
+	
+	
+
+	
