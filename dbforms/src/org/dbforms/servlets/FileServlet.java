c6b133159853be5503b53e15211fb06fe91cb13e
hkollmann <>
2005-02-15 20:58:23 +0000

    added null reference path from Luca

--- dbforms/src/org/dbforms/servlets/FileServlet.java
+++ dbforms/src/org/dbforms/servlets/FileServlet.java
@@ -35,7 +35,7 @@ import org.dbforms.config.Table;
 import org.dbforms.util.SqlUtil;
 import org.dbforms.util.StringUtil;
 import org.dbforms.util.Util;
-import org.dbforms.util.FileHolder; 
+import org.dbforms.util.FileHolder;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -51,173 +51,182 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 /**
  * #fixme - add appropriate exception-handling..
- *
+ * 
  * @author joe peer
  */
 public class FileServlet extends HttpServlet {
-   // logging category for this class
-   private static Log    logCat = LogFactory.getLog(FileServlet.class.getName());
+	// logging category for this class
+	private static Log logCat = LogFactory.getLog(FileServlet.class.getName());
 
-   /**
-    * Process the HTTP Get request
-    *
-    * @param request Description of the Parameter
-    * @param response Description of the Parameter
-    *
-    * @exception ServletException Description of the Exception
-    * @exception IOException Description of the Exception
-    */
-   public void doGet(HttpServletRequest  request,
-                     HttpServletResponse response)
-              throws ServletException, IOException {
+	/**
+	 * Process the HTTP Get request
+	 * 
+	 * @param request
+	 *            Description of the Parameter
+	 * @param response
+	 *            Description of the Parameter
+	 * 
+	 * @exception ServletException
+	 *                Description of the Exception
+	 * @exception IOException
+	 *                Description of the Exception
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    // take Config-Object from application context - this object should have
-    // been
-    // initalized by Config-Servlet on Webapp/server-startup!
-    DbFormsConfig config = null;
-   	try {
-       config = DbFormsConfigRegistry.instance()
-                                     .lookup();
-    } catch (Exception e) {
-       logCat.error(e);
-       throw new ServletException(e);
-    }
+		// take Config-Object from application context - this object should have
+		// been
+		// initalized by Config-Servlet on Webapp/server-startup!
+		DbFormsConfig config = null;
+		try {
+			config = DbFormsConfigRegistry.instance().lookup();
+		} catch (Exception e) {
+			logCat.error(e);
+			throw new ServletException(e);
+		}
 
-   	try {
-      	String tf           = request.getParameter("tf");
-         String keyValuesStr = request.getParameter("keyval");
-         int    tableId      = Integer.parseInt(StringUtil.getEmbeddedString(tf,
-                                                                            0,
-                                                                            '_'));
-         Table table   = config.getTable(tableId);
-         int   fieldId = Integer.parseInt(StringUtil.getEmbeddedString(tf, 1, '_'));
-         Field        field = table.getField(fieldId);
+		try {
+			String tf = request.getParameter("tf");
+			String keyValuesStr = request.getParameter("keyval");
+			if (!Util.isNull(keyValuesStr) && !("null".equals(keyValuesStr))) {
 
-         StringBuffer queryBuf         = new StringBuffer();
-         String       dbConnectionName = request.getParameter("invname_"
-                                                              + tableId);
-         Connection   con = config.getConnection(dbConnectionName);
+				int tableId = Integer.parseInt(StringUtil.getEmbeddedString(tf,
+						0, '_'));
+				Table table = config.getTable(tableId);
+				int fieldId = Integer.parseInt(StringUtil.getEmbeddedString(tf,
+						1, '_'));
+				Field field = table.getField(fieldId);
 
-         // JPeer 03/2004 - optional parameter
-         String      nameField = request.getParameter("nf");
+				StringBuffer queryBuf = new StringBuffer();
+				String dbConnectionName = request.getParameter("invname_"
+						+ tableId);
+				Connection con = config.getConnection(dbConnectionName);
 
-         InputStream is       = null;
-         String      fileName = null;
+				// JPeer 03/2004 - optional parameter
+				String nameField = request.getParameter("nf");
 
-         queryBuf.append("SELECT ");
-         queryBuf.append(field.getName());
+				InputStream is = null;
+				String fileName = null;
 
-         if (nameField != null) {
-            queryBuf.append(", ");
-            queryBuf.append(nameField);
-         }
+				queryBuf.append("SELECT ");
+				queryBuf.append(field.getName());
 
-         queryBuf.append(" FROM ");
-         queryBuf.append(table.getName());
-         queryBuf.append(" WHERE ");
-         queryBuf.append(table.getWhereClauseForKeyFields());
+				if (nameField != null) {
+					queryBuf.append(", ");
+					queryBuf.append(nameField);
+				}
 
-         // example: SELECT imageNameField FROM myTable WHERE myTable.key = ?
-         logCat.info("::doGet - query is [" + queryBuf + "]");
+				queryBuf.append(" FROM ");
+				queryBuf.append(table.getName());
+				queryBuf.append(" WHERE ");
+				queryBuf.append(table.getWhereClauseForKeyFields());
 
-         PreparedStatement ps = con.prepareStatement(queryBuf.toString());
-         table.populateWhereClauseWithKeyFields(keyValuesStr, ps, 1);
+				// example: SELECT imageNameField FROM myTable WHERE myTable.key
+				// = ?
+				logCat.info("::doGet - query is [" + queryBuf + "]");
 
-         ResultSet rs = ps.executeQuery();
+				PreparedStatement ps = con
+						.prepareStatement(queryBuf.toString());
+				table.populateWhereClauseWithKeyFields(keyValuesStr, ps, 1);
 
-         if (rs.next()) {
-            // use the filesystem;
-            if (field.getType() == FieldTypes.DISKBLOB) {
-               fileName = rs.getString(1);
-               is       = SqlUtil.readDiskBlob(fileName, field.getDirectory(),
-                                               request.getParameter("defaultValue"));
-            }
-            // use the rdbms;
-            else if (field.getType() == FieldTypes.BLOB) {
-               // if no fileholder is used (new BLOB model)
-               if (nameField != null) {
-                  fileName = rs.getString(2);
-                  is = SqlUtil.readDbFieldBlob(rs);
-               } else {
-                  FileHolder fh =  SqlUtil.readFileHolderBlob(rs);
-                  is = fh.getInputStreamFromBuffer();
-                  fileName = fh.getFileName();
-               }
-            }
-         } else {
-            logCat.info("::doGet - we have got no result using query "
-                        + queryBuf);
-         }
+				ResultSet rs = ps.executeQuery();
 
-         if (is != null) {
-            writeToClient(request, response, fileName, is);
-         }
+				if (rs.next()) {
+					// use the filesystem;
+					if (field.getType() == FieldTypes.DISKBLOB) {
+						fileName = rs.getString(1);
+						is = SqlUtil.readDiskBlob(fileName, field
+								.getDirectory(), request
+								.getParameter("defaultValue"));
+					}
+					// use the rdbms;
+					else if (field.getType() == FieldTypes.BLOB) {
+						// if no fileholder is used (new BLOB model)
+						if (nameField != null) {
+							fileName = rs.getString(2);
+							is = SqlUtil.readDbFieldBlob(rs);
+						} else {
+							FileHolder fh = SqlUtil.readFileHolderBlob(rs);
+							is = fh.getInputStreamFromBuffer();
+							fileName = fh.getFileName();
+						}
+					}
+				} else {
+					logCat.info("::doGet - we have got no result using query "
+							+ queryBuf);
+				}
 
-         SqlUtil.closeConnection(con);
-      } catch (SQLException sqle) {
-         logCat.error("::doGet - SQL exception", sqle);
-      }
-   }
+				if (is != null) {
+					writeToClient(request, response, fileName, is);
+				}
 
+				SqlUtil.closeConnection(con);
+			}
+		} catch (SQLException sqle) {
+			logCat.error("::doGet - SQL exception", sqle);
+		}
+	}
 
-   /**
-    * Process the HTTP Post request
-    *
-    * @param request Description of the Parameter
-    * @param response Description of the Parameter
-    *
-    * @exception ServletException Description of the Exception
-    * @exception IOException Description of the Exception
-    */
-   public void doPost(HttpServletRequest  request,
-                      HttpServletResponse response)
-               throws ServletException, IOException {
-      doGet(request, response);
-   }
+	/**
+	 * Process the HTTP Post request
+	 * 
+	 * @param request
+	 *            Description of the Parameter
+	 * @param response
+	 *            Description of the Parameter
+	 * 
+	 * @exception ServletException
+	 *                Description of the Exception
+	 * @exception IOException
+	 *                Description of the Exception
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
+	/**
+	 * Write the content of the input file to the client.
+	 * 
+	 * @param request
+	 *            DOCUMENT ME!
+	 * @param response
+	 *            Description of the Parameter
+	 * @param fileName
+	 *            Description of the Parameter
+	 * @param is
+	 *            Description of the Parameter
+	 * 
+	 * @exception IOException
+	 *                Description of the Exception
+	 */
+	private void writeToClient(HttpServletRequest request,
+			HttpServletResponse response, String fileName, InputStream is)
+			throws IOException {
+		String contentType = request.getSession().getServletContext()
+				.getMimeType(fileName);
+		logCat.info("::writeToClient- writing to client:" + fileName + " ct="
+				+ contentType);
 
+		if (!Util.isNull(contentType)) {
+			response.setContentType(contentType);
+		}
 
-   /**
-    * Write the content of the input file to the client.
-    *
-    * @param request DOCUMENT ME!
-    * @param response Description of the Parameter
-    * @param fileName Description of the Parameter
-    * @param is Description of the Parameter
-    *
-    * @exception IOException Description of the Exception
-    */
-   private void writeToClient(HttpServletRequest  request,
-                              HttpServletResponse response,
-                              String              fileName,
-                              InputStream         is) throws IOException {
-      String contentType = request.getSession()
-                                  .getServletContext()
-                                  .getMimeType(fileName);
-      logCat.info("::writeToClient- writing to client:" + fileName + " ct="
-                  + contentType);
+		response.setHeader("Cache-control", "private"); // w/o this MSIE fails
 
-      if (!Util.isNull(contentType)) {
-         response.setContentType(contentType);
-      }
+		// to "Open" the file
+		response.setHeader("Content-Disposition", "attachment; fileName=\""
+				+ fileName + "\"");
 
-      response.setHeader("Cache-control", "private"); // w/o this MSIE fails
+		ServletOutputStream out = response.getOutputStream();
+		byte[] b = new byte[1024];
+		int read;
 
-      // to "Open" the file
-      response.setHeader("Content-Disposition",
-                         "attachment; fileName=\"" + fileName + "\"");
+		while ((read = is.read(b)) != -1)
+			out.write(b, 0, read);
 
-      ServletOutputStream out  = response.getOutputStream();
-      byte[]              b    = new byte[1024];
-      int                 read;
-
-      while ((read = is.read(b)) != -1)
-         out.write(b, 0, read);
-
-      out.close();
-   }
+		out.close();
+	}
 }
