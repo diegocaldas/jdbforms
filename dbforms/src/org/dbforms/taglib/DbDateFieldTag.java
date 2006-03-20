52dd05e99d60b5c96a20bb593d36b1bfc31fd1c6
hkollmann <>
2006-03-20 20:26:12 +0000

    set classic calendar as default

--- dbforms/src/org/dbforms/taglib/DbDateFieldTag.java
+++ dbforms/src/org/dbforms/taglib/DbDateFieldTag.java
@@ -26,137 +26,154 @@ package org.dbforms.taglib;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 
-
-
 /**
  * <p>
  * This tag renders a HTML TextArea - Element
  * </p>
  * this tag renders a dabase-datadriven textArea, which is an active element -
  * the user can change data
- *
+ * 
  * @author Joachim Peer
  */
-public class DbDateFieldTag extends AbstractDbBaseInputTag
-   implements javax.servlet.jsp.tagext.TryCatchFinally {
+public class DbDateFieldTag extends AbstractDbBaseInputTag implements javax.servlet.jsp.tagext.TryCatchFinally {
    /** Holds value of property jsCalendarDateFormat. */
    private String jsCalendarDateFormat;
 
    /** Holds value of property useJsCalendar. */
    private String useJsCalendar;
-   
+
    /** Determinate whether the classic javascript calendar should be used */
-   private String classicCalendar = "false";
+   private String classicCalendar = "true";
 
    /**
     * Setter for property jsCalendarDateFormat.
-    *
-    * @param jsCalendarDateFormat New value of property jsCalendarDateFormat.
+    * 
+    * @param jsCalendarDateFormat
+    *           New value of property jsCalendarDateFormat.
     */
    public void setJsCalendarDateFormat(String jsCalendarDateFormat) {
       this.jsCalendarDateFormat = jsCalendarDateFormat;
    }
 
-
    /**
     * Getter for property jsCalendarDateFormat.
-    *
+    * 
     * @return Value of property jsCalendarDateFormat.
     */
    public String getJsCalendarDateFormat() {
       return jsCalendarDateFormat;
    }
 
-
    /**
     * Setter for property useJsCalendar.
-    *
-    * @param useJsCalendar New value of property useJsCalendar.
+    * 
+    * @param useJsCalendar
+    *           New value of property useJsCalendar.
     */
    public void setUseJsCalendar(String useJsCalendar) {
       this.useJsCalendar = useJsCalendar;
    }
 
-
    /**
     * Getter for property useJsCalendar.
-    *
+    * 
     * @return Value of property useJsCalendar.
     */
    public String getUseJsCalendar() {
       return useJsCalendar;
    }
 
-
-   /** 
-    * process the display of the classic javascript calendar 
-    * @param request the request to process
-    * @param tagBuf the buffer containing the rendered html
-    * 						nicolas parise 2006-02-11 						
-    * */
-   private void processClassicCalendar(HttpServletRequest request,StringBuffer tagBuf) throws JspException {
-       // if property useJSCalendar is set to 'true' we will now add a little
-       // image that can be clicked to popup a small JavaScript Calendar
-       // written by Robert W. Husted to edit the field:
-       if (!hasHiddenSet() && ("true".equals(this.getUseJsCalendar()))) {
-          tagBuf.append(" <a href=\"javascript:doNothing()\" ")
-                .append(" onclick=\"");
-
-          // if date format is not explicitely set for this calendar,
-          // use date format for this form field. 
-          if ((jsCalendarDateFormat == null)
-                    && (getFormatter() instanceof java.text.SimpleDateFormat)) {
-             java.text.SimpleDateFormat mysdf = (java.text.SimpleDateFormat) getFormatter();
-             jsCalendarDateFormat = mysdf.toPattern();
-
-             // 2 digit date format pattern is 'dd' in Java, 'DD' in
-             // JavaScript calendar
-             if (jsCalendarDateFormat.indexOf("dd") >= 0) {
-                jsCalendarDateFormat = jsCalendarDateFormat.replace('d', 'D');
-             }
-          }
-
-          if (jsCalendarDateFormat != null) // JS Date Format set ?
-           {
-             tagBuf.append("calDateFormat='" + jsCalendarDateFormat + "';");
-          }
-
-          tagBuf.append("setDateField(document.dbform['")
-                .append(getFormFieldName())
-                .append("']);")
-                .append(" top.newWin = window.open('")
-                .append(request.getContextPath())
-                .append("/dbformslib/jscal/calendar.html','cal','width=270,height=280')\">")
-                .append("<img src=\"")
-                .append(request.getContextPath())
-                .append("/dbformslib/jscal/calendar.gif\"  ") //width=\"16\" height=\"16\"
-
-                .append(" border=\"0\"  alt=\"Click on the Calendar to activate the Pop-Up Calendar Window.\">")
-                .append("</img>")
-                .append("</a>");
-       }
-
-       // For generation Javascript Validation.  Need all original and modified fields name
-       getParentForm()
-          .addChildName(getName(), getFormFieldName());
+   private void processNewCalendar(HttpServletRequest request, StringBuffer tagBuf) throws JspException {
+      if (!hasHiddenSet() && ("true".equals(this.getUseJsCalendar()))) {
+         tagBuf.append(" <a href=\"javascript:void(0);\" ").append(" onclick=\"");
+         // if date format is not explicitely set for this calendar,
+         // use date format for this form field.
+         if ((jsCalendarDateFormat == null) && (getFormatter() instanceof java.text.SimpleDateFormat)) {
+            java.text.SimpleDateFormat mysdf = (java.text.SimpleDateFormat) getFormatter();
+            jsCalendarDateFormat = parseDateFormatPattern(mysdf.toPattern());
+         }
+         tagBuf.append("return showCalendar('")
+               .append(getFormFieldName())
+               .append("', '")
+               .append(jsCalendarDateFormat)
+               .append("', '24', true);\">")
+               .append("<img src=\"")
+               .append(request.getContextPath())
+               .append("/dbformslib/jscal2/calendar.gif\"  ") 
+               .append(" border=\"0\"  alt=\"Click on the Calendar to activate the Pop-Up Calendar Window.\">")
+               .append("</img>")
+               .append("</a>");
+      }
    }
-   
+
+   /**
+    * process the display of the classic javascript calendar
+    * 
+    * @param request
+    *           the request to process
+    * @param tagBuf
+    *           the buffer containing the rendered html nicolas parise
+    *           2006-02-11
+    */
+   private void processClassicCalendar(HttpServletRequest request, StringBuffer tagBuf) throws JspException {
+      // if property useJSCalendar is set to 'true' we will now add a little
+      // image that can be clicked to popup a small JavaScript Calendar
+      // written by Robert W. Husted to edit the field:
+      if (!hasHiddenSet() && ("true".equals(this.getUseJsCalendar()))) {
+         tagBuf.append(" <a href=\"javascript:doNothing()\" ").append(" onclick=\"");
+
+         // if date format is not explicitely set for this calendar,
+         // use date format for this form field.
+         if ((jsCalendarDateFormat == null) && (getFormatter() instanceof java.text.SimpleDateFormat)) {
+            java.text.SimpleDateFormat mysdf = (java.text.SimpleDateFormat) getFormatter();
+            jsCalendarDateFormat = mysdf.toPattern();
+
+            // 2 digit date format pattern is 'dd' in Java, 'DD' in
+            // JavaScript calendar
+            if (jsCalendarDateFormat.indexOf("dd") >= 0) {
+               jsCalendarDateFormat = jsCalendarDateFormat.replace('d', 'D');
+            }
+         }
+         //       JS Date Format set ?
+         if (jsCalendarDateFormat != null)  {
+            tagBuf.append("calDateFormat='" + jsCalendarDateFormat + "';");
+         }
+
+         tagBuf.append("setDateField(document.dbform['")
+         .append(getFormFieldName())
+         .append("']);")
+         .append(" top.newWin = window.open('")
+         .append(request.getContextPath())
+         .append("/dbformslib/jscal/calendar.html','cal','width=270,height=280')\">")
+         .append("<img src=\"")
+         .append(request.getContextPath())
+         .append("/dbformslib/jscal/calendar.gif\"  ") 
+         .append(" border=\"0\"  alt=\"Click on the Calendar to activate the Pop-Up Calendar Window.\">")
+         .append("</img>")
+         .append("</a>");
+      }
+
+      // For generation Javascript Validation. Need all original and modified
+      // fields name
+      getParentForm().addChildName(getName(), getFormFieldName());
+   }
+
    /**
     * DOCUMENT ME!
-    *
+    * 
     * @return DOCUMENT ME!
-    *
-    * @throws javax.servlet.jsp.JspException DOCUMENT ME!
-    * @throws JspException DOCUMENT ME!
+    * 
+    * @throws javax.servlet.jsp.JspException
+    *            DOCUMENT ME!
+    * @throws JspException
+    *            DOCUMENT ME!
     */
    public int doEndTag() throws JspException {
-      HttpServletRequest request = (HttpServletRequest) this.pageContext
-                                   .getRequest();
+      HttpServletRequest request = (HttpServletRequest) this.pageContext.getRequest();
 
       try {
-         
-    	  
-   	     StringBuffer tagBuf = new StringBuffer("<input ");
+
+         StringBuffer tagBuf = new StringBuffer("<input ");
          tagBuf.append(prepareType());
          tagBuf.append(prepareName());
          tagBuf.append(prepareValue());
@@ -166,39 +183,14 @@ public class DbDateFieldTag extends AbstractDbBaseInputTag
          tagBuf.append(prepareEventHandlers());
          tagBuf.append("/>");
 
-         
-   	  if ("true".equals(classicCalendar)) {
-		  processClassicCalendar(request,tagBuf);
-	  }
-   	  else {
-          if (!hasHiddenSet() && ("true".equals(this.getUseJsCalendar()))) {
-         	 tagBuf.append(" <a href=\"javascript:void(0);\" ")
-                   .append(" onclick=\"");
-             // if date format is not explicitely set for this calendar,
-             // use date format for this form field. 
-             if ((jsCalendarDateFormat == null)
-                       && (getFormatter() instanceof java.text.SimpleDateFormat)) {
-                java.text.SimpleDateFormat mysdf = (java.text.SimpleDateFormat) getFormatter();
-                jsCalendarDateFormat = parseDateFormatPattern(mysdf.toPattern());
-             }
-             tagBuf.append("return showCalendar('")
-                   .append(getFormFieldName())
-                   .append("', '")
-                   .append(jsCalendarDateFormat)
-                   .append("', '24', true);\">")
-                   .append("<img src=\"")
-                   .append(request.getContextPath())
-                   .append("/dbformslib/jscal2/calendar.gif\"  ") //width=\"16\" height=\"16\"
-                   .append(" border=\"0\"  alt=\"Click on the Calendar to activate the Pop-Up Calendar Window.\">")
-                   .append("</img>")
-                   .append("</a>");
-          }
-   	  }
+         if ("true".equals(classicCalendar)) {
+            processClassicCalendar(request, tagBuf);
+         } else {
+            processNewCalendar(request, tagBuf);
 
-         
+         }
 
-         pageContext.getOut()
-                    .write(tagBuf.toString());
+         pageContext.getOut().write(tagBuf.toString());
 
          // Writes out the old field value
          writeOutSpecialValues();
@@ -209,7 +201,6 @@ public class DbDateFieldTag extends AbstractDbBaseInputTag
       return EVAL_PAGE;
    }
 
-
    /**
     * DOCUMENT ME!
     */
@@ -217,13 +208,13 @@ public class DbDateFieldTag extends AbstractDbBaseInputTag
       super.doFinally();
    }
 
-
    /**
     * DOCUMENT ME!
-    *
+    * 
     * @return DOCUMENT ME!
-    *
-    * @throws javax.servlet.jsp.JspException DOCUMENT ME!
+    * 
+    * @throws javax.servlet.jsp.JspException
+    *            DOCUMENT ME!
     */
    public int doStartTag() throws JspException {
       super.doStartTag();
@@ -234,107 +225,107 @@ public class DbDateFieldTag extends AbstractDbBaseInputTag
    /**
     * Return the JavaScript Calendar pattern for the given Java DateFormat
     * pattern.
+    * 
     * @author Malcolm A. Edgar
-    * @param pattern the Java DateFormat pattern
+    * @param pattern
+    *           the Java DateFormat pattern
     * @return JavaScript Calendar pattern
     */
    private String parseDateFormatPattern(String pattern) {
-       StringBuffer jsPattern = new StringBuffer(20);
-       int tokenStart = -1;
-       int tokenEnd = -1;
-       //boolean debug = false;
+      StringBuffer jsPattern = new StringBuffer(20);
+      int tokenStart = -1;
+      int tokenEnd = -1;
+      // boolean debug = false;
 
-       for (int i = 0; i < pattern.length(); i++) {
-           char aChar = pattern.charAt(i);
+      for (int i = 0; i < pattern.length(); i++) {
+         char aChar = pattern.charAt(i);
 
-           // If character is in SimpleDateFormat pattern character set
-           if ("GyMwWDdFEaHkKhmsSzZ".indexOf(aChar) == -1) {
-               if (tokenStart > -1) {
-                   tokenEnd = i;
+         // If character is in SimpleDateFormat pattern character set
+         if ("GyMwWDdFEaHkKhmsSzZ".indexOf(aChar) == -1) {
+            if (tokenStart > -1) {
+               tokenEnd = i;
+            }
+         } else {
+            if (tokenStart == -1) {
+               tokenStart = i;
+            }
+         }
+
+         if (tokenStart > -1) {
+
+            if (tokenEnd == -1 && i == pattern.length() - 1) {
+               tokenEnd = pattern.length();
+            }
+
+            if (tokenEnd > -1) {
+               String token = pattern.substring(tokenStart, tokenEnd);
+
+               if ("yyyy".equals(token)) {
+                  jsPattern.append("%Y");
+               } else if ("yy".equals(token)) {
+                  jsPattern.append("%y");
+               } else if ("MMMM".equals(token)) {
+                  jsPattern.append("%B");
+               } else if ("MMM".equals(token)) {
+                  jsPattern.append("%b");
+               } else if ("MM".equals(token)) {
+                  jsPattern.append("%m");
+               } else if ("dd".equals(token)) {
+                  jsPattern.append("%d");
+               } else if ("d".equals(token)) {
+                  jsPattern.append("%e");
+               } else if ("EEEE".equals(token)) {
+                  jsPattern.append("%A");
+               } else if ("EEE".equals(token)) {
+                  jsPattern.append("%a");
+               } else if ("EE".equals(token)) {
+                  jsPattern.append("%a");
+               } else if ("E".equals(token)) {
+                  jsPattern.append("%a");
+               } else if ("aaa".equals(token)) {
+                  jsPattern.append("%p");
+               } else if ("aa".equals(token)) {
+                  jsPattern.append("%p");
+               } else if ("a".equals(token)) {
+                  jsPattern.append("%p");
+               } else if ("HH".equals(token)) {
+                  jsPattern.append("%H");
+               } else if ("H".equals(token)) {
+                  jsPattern.append("%H");
+               } else if ("hh".equals(token)) {
+                  jsPattern.append("%l");
+               } else if ("h".equals(token)) {
+                  jsPattern.append("%l");
+               } else if ("mm".equals(token)) {
+                  jsPattern.append("%M");
+               } else if ("m".equals(token)) {
+                  jsPattern.append("%M");
+               } else if ("ss".equals(token)) {
+                  jsPattern.append("%S");
+               } else if ("s".equals(token)) {
+                  jsPattern.append("%S");
                }
-           } else {
-               if (tokenStart == -1) {
-                   tokenStart = i;
-               }
-           }
 
-           if (tokenStart > -1) {
+               tokenStart = -1;
+               tokenEnd = -1;
+            }
+         }
 
-               if (tokenEnd == -1 && i == pattern.length() -1) {
-                   tokenEnd = pattern.length();
-               }
+         if (tokenStart == -1 && tokenEnd == -1) {
+            if ("GyMwWDdFEaHkKhmsSzZ".indexOf(aChar) == -1) {
+               jsPattern.append(aChar);
+            }
+         }
+      }
 
-               if (tokenEnd > -1) {
-                   String token = pattern.substring(tokenStart, tokenEnd);
-
-                   if ("yyyy".equals(token)) {
-                       jsPattern.append("%Y");
-                   } else if ("yy".equals(token)) {
-                       jsPattern.append("%y");
-                   } else if ("MMMM".equals(token)) {
-                       jsPattern.append("%B");
-                   } else if ("MMM".equals(token)) {
-                       jsPattern.append("%b");
-                   } else if ("MM".equals(token)) {
-                       jsPattern.append("%m");
-                   } else if ("dd".equals(token)) {
-                       jsPattern.append("%d");
-                   } else if ("d".equals(token)) {
-                       jsPattern.append("%e");
-                   } else if ("EEEE".equals(token)) {
-                       jsPattern.append("%A");
-                   } else if ("EEE".equals(token)) {
-                       jsPattern.append("%a");
-                   } else if ("EE".equals(token)) {
-                       jsPattern.append("%a");
-                   } else if ("E".equals(token)) {
-                       jsPattern.append("%a");
-                   } else if ("aaa".equals(token)) {
-                       jsPattern.append("%p");
-                   } else if ("aa".equals(token)) {
-                       jsPattern.append("%p");
-                   } else if ("a".equals(token)) {
-                       jsPattern.append("%p");
-                   } else if ("HH".equals(token)) {
-                       jsPattern.append("%H");
-                   } else if ("H".equals(token)) {
-                       jsPattern.append("%H");
-                   } else if ("hh".equals(token)) {
-                       jsPattern.append("%l");
-                   } else if ("h".equals(token)) {
-                       jsPattern.append("%l");
-                   } else if ("mm".equals(token)) {
-                       jsPattern.append("%M");
-                   } else if ("m".equals(token)) {
-                       jsPattern.append("%M");
-                   } else if ("ss".equals(token)) {
-                       jsPattern.append("%S");
-                   } else if ("s".equals(token)) {
-                       jsPattern.append("%S");
-                   } 
-
-                   tokenStart = -1;
-                   tokenEnd = -1;
-               }
-           }
-
-           if (tokenStart == -1 && tokenEnd == -1) {
-               if ("GyMwWDdFEaHkKhmsSzZ".indexOf(aChar) == -1) {
-                   jsPattern.append(aChar);
-               }
-           }
-       }
-
-       return jsPattern.toString();
+      return jsPattern.toString();
    }
 
+   public String getClassicCalendar() {
+      return classicCalendar;
+   }
 
-public String getClassicCalendar() {
-	return classicCalendar;
-}
-
-
-public void setClassicCalendar(String classicCalendar) {
-	this.classicCalendar = classicCalendar;
-}
+   public void setClassicCalendar(String classicCalendar) {
+      this.classicCalendar = classicCalendar;
+   }
 }
